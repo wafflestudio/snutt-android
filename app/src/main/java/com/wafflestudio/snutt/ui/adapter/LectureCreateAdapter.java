5a63f36500b567b17c81240a8292190a56c116ff
Seongwon Bang <makesource@wafflestudio.com>
2016-09-25 02:23:53 +0900

    애니메이션 관련 삽질...의 결과물

--- app/src/main/java/com/wafflestudio/snutt/ui/adapter/LectureCreateAdapter.java
+++ app/src/main/java/com/wafflestudio/snutt/ui/adapter/LectureCreateAdapter.java
@@ -1,20 +1,28 @@
 package com.wafflestudio.snutt.ui.adapter;
 
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.widget.DrawerLayout;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.wafflestudio.snutt.R;
+import com.wafflestudio.snutt.model.ClassTime;
 import com.wafflestudio.snutt.model.LectureItem;
+import com.wafflestudio.snutt.ui.LectureMainActivity;
 
 import java.util.ArrayList;
 
@@ -26,6 +34,7 @@ public class LectureCreateAdapter extends BaseAdapter {
     private Activity activity;
     private ArrayList<LectureItem> lists;
     private LayoutInflater inflater;
+    private boolean isAnimated = true;
 
     private final static String TAG = "LECTURE_CREATE_ADAPTER";
     private final static int TYPE_HEADER = 0;
@@ -38,7 +47,7 @@ public class LectureCreateAdapter extends BaseAdapter {
     public LectureCreateAdapter(Activity activity, ArrayList<LectureItem> lists) {
         this.activity = activity;
         this.lists = lists;
-        this.inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -92,8 +101,6 @@ public class LectureCreateAdapter extends BaseAdapter {
                 EditText value = (EditText) view.findViewById(R.id.text_value);
                 title.setText(item.getTitle1());
                 value.setText(item.getValue1());
-                value.setClickable(item.isEditable());
-                value.setFocusable(item.isEditable());
                 value.addTextChangedListener(new TextWatcher() {
                     @Override
                     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -108,43 +115,8 @@ public class LectureCreateAdapter extends BaseAdapter {
                         item.setValue1(s.toString());
                     }
                 });
-                break;
-            }
-            case TYPE_ITEM_DETAIL: {
-                TextInputLayout title1 = (TextInputLayout) view.findViewById(R.id.input_title1);
-                EditText editText1 = (EditText) view.findViewById(R.id.input_detail1);
-                title1.setHint(item.getTitle1());
-                editText1.setText(item.getValue1());
-                editText1.addTextChangedListener(new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {}
-                    @Override
-                    public void afterTextChanged(Editable s) {
-                        item.setValue1(s.toString());
-                    }
-                });
-                TextInputLayout title2 = (TextInputLayout) view.findViewById(R.id.input_title2);
-                EditText editText2 = (EditText) view.findViewById(R.id.input_detail2);
-                title2.setHint(item.getTitle2());
-                editText2.setText(item.getValue2());
-                editText2.addTextChangedListener(new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {}
-                    @Override
-                    public void afterTextChanged(Editable s) {
-                        item.setValue2(s.toString());
-                    }
-                });
-                editText1.setClickable(item.isEditable());
-                editText1.setFocusable(item.isEditable());
-                editText2.setClickable(item.isEditable());
-                editText2.setFocusable(item.isEditable());
-                if (position == 6) { // 학점
-                    editText2.setInputType(InputType.TYPE_CLASS_NUMBER);
+                if (position == 4) { // 학점
+                    value.setInputType(InputType.TYPE_CLASS_NUMBER);
                 }
                 break;
             }
@@ -154,13 +126,64 @@ public class LectureCreateAdapter extends BaseAdapter {
                 textView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
+                        addClassItem();
+                        isAnimated = false;
+                        notifyDataSetChanged();
                     }
                 });
                 break;
             }
+            case TYPE_ITEM_COLOR: {
+                LinearLayout layout = (LinearLayout) view.findViewById(R.id.layout);
+                TextView title = (TextView) view.findViewById(R.id.text_title);
+                View fgColor = (View) view.findViewById(R.id.fgColor);
+                View bgColor = (View) view.findViewById(R.id.bgColor);
+                title.setText("색상");
+                layout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (item.isEditable()) {
+                            ((LectureMainActivity) activity).setColorPickerFragment();
+                        }
+                    }
+                });
+                bgColor.setBackgroundColor(item.getColor().getBg());
+                fgColor.setBackgroundColor(item.getColor().getFg());
+                break;
+            }
+            case TYPE_ITEM_CLASS: {
+                final EditText editText1 = (EditText) view.findViewById(R.id.input_time);
+                final EditText editText2 = (EditText) view.findViewById(R.id.input_location);
+                if (position == getCount() - 2) {
+                    if (!isAnimated) {
+                        editText1.setVisibility(View.GONE);
+                        editText2.setVisibility(View.GONE);
+                        final DisplayMetrics dm = activity.getResources().getDisplayMetrics();
+                        ValueAnimator va = ValueAnimator.ofInt(0, (int)(60 * dm.density));
+                        final View finalView = view;
+                        va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                int value = (Integer) animation.getAnimatedValue();
+                                finalView.getLayoutParams().height = value;
+                                finalView.requestLayout();
+                                if (value == (int) (60 * dm.density)) {
+                                    editText1.setVisibility(View.VISIBLE);
+                                    editText2.setVisibility(View.VISIBLE);
+                                }
+                            }
+                        });
+                        va.setDuration(400);
+                        va.start();
+                        isAnimated = true;
+                    }
+                }
+            }
         }
-
         return view;
+    }
+
+    private void addClassItem() {
+        int pos = getCount() - 1;
+        lists.add(pos, new LectureItem(new ClassTime(1,1,1," "), LectureItem.Type.ItemClass));
     }
 }
