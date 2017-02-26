2e634f81b5225782b7e961d9eb569c0d1bca570b
Seongwon Bang <makesource@wafflestudio.com>
2017-02-27 00:45:20 +0900

    add headers in webview

--- app/src/main/java/com/wafflestudio/snutt/ui/TermsFragment.java
+++ app/src/main/java/com/wafflestudio/snutt/ui/TermsFragment.java
@@ -9,6 +9,10 @@ import android.webkit.WebViewClient;
 
 import com.wafflestudio.snutt.R;
 import com.wafflestudio.snutt.SNUTTBaseFragment;
+import com.wafflestudio.snutt.manager.PrefManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by makesource on 2017. 1. 24..
@@ -16,14 +20,18 @@ import com.wafflestudio.snutt.SNUTTBaseFragment;
 
 public class TermsFragment extends SNUTTBaseFragment {
     private WebView webView;
+    private Map headers;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_privacy, container, false);
+        headers = new HashMap();
+        headers.put("x-access-apikey", getResources().getString(R.string.api_key));
+        headers.put("x-access-token", PrefManager.getInstance().getPrefKeyXAccessToken());
         webView = (WebView) rootView.findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient()); // 이걸 안해주면 새창이 뜸
-        webView.loadUrl(getString(R.string.api_server) + getString(R.string.terms));
+        webView.loadUrl(getString(R.string.api_server) + getString(R.string.terms), headers);
         return rootView;
     }
 }
