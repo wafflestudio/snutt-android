517a7dd93ab63e50662d033197bee511705c4c5c
Seongwon Bang <makesource@wafflestudio.com>
2017-02-27 00:55:44 +0900

    ready for receive notification list

--- app/src/main/java/com/wafflestudio/snutt/model/Notification.java
+++ app/src/main/java/com/wafflestudio/snutt/model/Notification.java
@@ -9,4 +9,27 @@ public class Notification {
     private String message;
     private int type;
 
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getId() {
+        return _id;
+    }
+
+    public void setId(String _id) {
+        this._id = _id;
+    }
 }
