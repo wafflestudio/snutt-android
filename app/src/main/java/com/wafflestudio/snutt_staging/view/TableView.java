f5a6b580d071ed34fab1651d6abd28367d2268fc
Seongwon Bang <makesource@wafflestudio.com>
2017-04-02 22:55:51 +0900

    drawPath 의 성능문제로 rollback

--- app/src/main/java/com/wafflestudio/snutt_staging/view/TableView.java
+++ app/src/main/java/com/wafflestudio/snutt_staging/view/TableView.java
@@ -35,7 +35,7 @@ public class TableView extends View {
     private static String TAG = "VIEW_TAG_TABLE_VIEW";
 
     private Paint backgroundPaint;
-    private Paint linePaint, linePaint2, topLabelTextPaint, leftLabelTextPaint, leftLabelTextPaint2;
+    private Paint linePaint, topLabelTextPaint, leftLabelTextPaint, leftLabelTextPaint2;
     private Paint lectureTextPaint;
     private Path path;
     private Context mContext;
@@ -86,12 +86,6 @@ public class TableView extends View {
         linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         linePaint.setColor(0xffcccccc);
         linePaint.setStrokeWidth(1);
-
-        linePaint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
-        linePaint2.setStyle(Paint.Style.STROKE);
-        linePaint2.setColor(0xffcccccc);
-        linePaint2.setStrokeWidth(1);
-        linePaint2.setPathEffect(new DashPathEffect(new float[]{5,5}, 2));
 
         path = new Path();
 
@@ -322,7 +316,8 @@ public class TableView extends View {
             if (i%2 == 1) {
                 path.moveTo(leftLabelWidth, height);
                 path.lineTo(canvasWidth, height);
-                canvas.drawPath(path, linePaint2);
+                //canvas.drawPath(path, linePaint2);
+                canvas.drawLine(leftLabelWidth, height,canvasWidth, height,linePaint);
             }
             else {
                 canvas.drawLine(leftLabelWidth / 3f, height, canvasWidth, height, linePaint);
