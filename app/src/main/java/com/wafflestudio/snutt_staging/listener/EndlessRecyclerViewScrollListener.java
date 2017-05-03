a7676827a222c8f3a0a38b230a6177f0e01bac8a
Seongwon Bang <makesource@wafflestudio.com>
2017-05-04 01:41:20 +0900

    endless scrollview 반응성 향상

--- app/src/main/java/com/wafflestudio/snutt_staging/listener/EndlessRecyclerViewScrollListener.java
+++ app/src/main/java/com/wafflestudio/snutt_staging/listener/EndlessRecyclerViewScrollListener.java
@@ -91,6 +91,7 @@ public abstract class EndlessRecyclerViewScrollListener extends RecyclerView.OnS
                 // If we do need to reload some more data, we execute onLoadMore to fetch the data.
                 // threshold should reflect how many total columns there are too
 
+                if (previousTotalItemCount == totalItemCount) return;
                 previousTotalItemCount = totalItemCount;
                 currentPage++;
                 onLoadMore(currentPage, totalItemCount);
