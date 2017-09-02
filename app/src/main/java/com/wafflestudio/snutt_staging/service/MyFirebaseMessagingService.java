15fcd3d74a590ca423ce3743e068ec9706265fa7
Seongwon Bang <makesource@wafflestudio.com>
2017-09-02 22:29:22 +0900

    Notification 아이콘 수정

--- app/src/main/java/com/wafflestudio/snutt_staging/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/wafflestudio/snutt_staging/service/MyFirebaseMessagingService.java
@@ -74,7 +74,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.search_icon)
+                .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("SNUTT")
                 .setContentText(messageBody)
                 .setAutoCancel(true)
