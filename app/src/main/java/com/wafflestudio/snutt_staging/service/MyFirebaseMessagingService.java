76bfb7997fe7f995f8b45fd3a6af2b9573b317a0
Seongwon Bang <makesource@wafflestudio.com>
2017-09-24 16:02:21 +0900

    Launcer icon, Notification icon 변경

--- app/src/main/java/com/wafflestudio/snutt_staging/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/wafflestudio/snutt_staging/service/MyFirebaseMessagingService.java
@@ -74,7 +74,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.drawable.ic_notification)
                 .setContentTitle("SNUTT")
                 .setContentText(messageBody)
                 .setAutoCancel(true)
