0475594b36e0936f86759c9ffad6faa56eb4ea8a
Seongwon Bang <makesource@wafflestudio.com>
2017-12-22 00:12:20 +0900

    handle Preference null string

--- app/src/main/java/com/wafflestudio/snutt2/manager/PrefManager.java
+++ app/src/main/java/com/wafflestudio/snutt2/manager/PrefManager.java
@@ -74,7 +74,7 @@ public class PrefManager {
         editor.apply();
     }
     public String getLastViewTableId() {
-        return sp.getString(PREF_KEY_LAST_VIEW_TABLE_ID, null);
+        return sp.getString(PREF_KEY_LAST_VIEW_TABLE_ID, "");
     }
 
     public void setPrefKeyXAccessToken(String token) {
