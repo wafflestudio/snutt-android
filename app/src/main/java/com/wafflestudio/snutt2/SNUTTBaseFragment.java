2ca225f6dcf33ad4c8695e40183b087eda088062
Seongwon Bang <makesource@wafflestudio.com>
2017-12-21 23:23:41 +0900

    bugfix in SNUTTBaseFragment.java

--- app/src/main/java/com/wafflestudio/snutt2/SNUTTBaseFragment.java
+++ app/src/main/java/com/wafflestudio/snutt2/SNUTTBaseFragment.java
@@ -15,28 +15,43 @@ import com.wafflestudio.snutt2.ui.MainActivity;
  */
 public class SNUTTBaseFragment extends Fragment {
 
+    private Activity mActivity;
     private static final String TAG = "SNUTT_BASE_FRAGMENT";
     public static final String INTENT_KEY_TABLE_ID = "INTENT_KEY_TABLE_ID";
     public static final String INTENT_KEY_LECTURE_POSITION = "INTENT_KEY_LECTURE_POSITION";
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof Activity){
+            mActivity = (Activity) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mActivity = null;
+    }
+
     protected SNUTTApplication getApp() {
-        return (SNUTTApplication) getActivity().getApplication();
+        return (SNUTTApplication) mActivity.getApplication();
     }
 
     public MainActivity getMainActivity() {
-        Activity activity = getActivity();
+        Activity activity = mActivity;
         Preconditions.checkState(activity instanceof MainActivity);
         return (MainActivity) activity;
     }
 
     public SNUTTBaseActivity getBaseActivity() {
-        Activity activity = getActivity();
+        Activity activity = mActivity;
         Preconditions.checkState(activity instanceof SNUTTBaseActivity);
         return (SNUTTBaseActivity) activity;
     }
 
     protected void hideSoftKeyboard(View view) {
-        InputMethodManager mgr = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        InputMethodManager mgr = (InputMethodManager) mActivity.getSystemService(Context.INPUT_METHOD_SERVICE);
         mgr.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
