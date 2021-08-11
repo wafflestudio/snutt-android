ef5e92813ce576bd77d87f4eed97cc2d2831eb0d
sanggggg <37951125+sanggggg@users.noreply.github.com>
2021-08-11 22:56:06 +0900

    :bug: PostFeedbackResults API 명세 반영 (#23)

--- app/src/main/java/com/wafflestudio/snutt2/lib/network/dto/PostFeedbackResults.kt
+++ app/src/main/java/com/wafflestudio/snutt2/lib/network/dto/PostFeedbackResults.kt
@@ -2,8 +2,7 @@ package com.wafflestudio.snutt2.lib.network.dto
 
 import com.squareup.moshi.JsonClass
 
-// Refactoring FIXME: api 명세 없음
 @JsonClass(generateAdapter = true)
 data class PostFeedbackResults(
-    val asdf: Any
+    val message: String? = null
 )
