cf22724c2c31a8a72f45c9b5a128b46a6a8fcf8d
sanggggg <ksme6776@gmail.com>
2021-10-02 15:56:33 +0900

    :sparkles: current table repository 에 preview theme 추가하기

--- app/src/main/java/com/wafflestudio/snutt2/data/current_table/CurrentTableRepository.kt
+++ app/src/main/java/com/wafflestudio/snutt2/data/current_table/CurrentTableRepository.kt
@@ -1,5 +1,6 @@
 package com.wafflestudio.snutt2.data.current_table
 
+import com.wafflestudio.snutt2.data.TimetableColorTheme
 import com.wafflestudio.snutt2.lib.network.dto.PostCustomLectureParams
 import com.wafflestudio.snutt2.lib.network.dto.PutLectureParams
 import com.wafflestudio.snutt2.lib.network.dto.core.TableDto
@@ -7,6 +8,8 @@ import kotlinx.coroutines.flow.Flow
 
 interface CurrentTableRepository {
     val currentTable: Flow<TableDto>
+
+    val previewTheme: Flow<TimetableColorTheme?>
 
     suspend fun addLecture(lectureId: String)
 
@@ -19,6 +22,8 @@ interface CurrentTableRepository {
 
     // FIXME: do not expose network layer data class
     suspend fun updateLecture(lectureId: String, target: PutLectureParams)
+
+    suspend fun setPreviewTheme(previewTheme: TimetableColorTheme?)
 
     suspend fun getLectureSyllabusUrl(
         courseNumber: String,
