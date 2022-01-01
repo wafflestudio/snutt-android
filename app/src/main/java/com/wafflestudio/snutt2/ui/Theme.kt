bc93e74f78def8c7fd0ae7354f3a40943fffb408
sanggggg <37951125+sanggggg@users.noreply.github.com>
2022-01-01 18:03:30 +0900

    :boom: Setup Jetpack Compose & Add Compose Migration Sample (#35)

---
+++ app/src/main/java/com/wafflestudio/snutt2/ui/Theme.kt
@@ -0,0 +1,30 @@
+package com.wafflestudio.snutt2.ui
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightThemeColors = lightColors(
+    primary = Black900,
+    primaryVariant = Gray400,
+    onPrimary = Color.White,
+    secondary = Black900,
+    secondaryVariant = Gray400,
+    onSecondary = Color.White,
+    error = White900,
+    onBackground = Color.Black,
+)
+
+@Composable
+fun SnuttTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = LightThemeColors,
+        typography = SnuttTypography,
+        content = content
+    )
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.4")
         classpath("com.google.gms:google-services:4.3.10")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.36")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
     }
 }
