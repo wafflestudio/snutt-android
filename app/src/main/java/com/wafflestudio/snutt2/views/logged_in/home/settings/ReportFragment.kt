9481bfc4fb4a379c6db7932515f67c3cda6ae03c
sanggggg <ksme6776@gmail.com>
2022-01-22 20:19:11 +0900

    require email for report

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/ReportFragment.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/ReportFragment.kt
@@ -48,23 +48,29 @@ class ReportFragment : BaseFragment() {
             .bindUi(this) {
                 if (binding.detailEditText.text.toString().isEmpty()) {
                     Toast.makeText(requireContext(), "내용을 입력해 주세요", Toast.LENGTH_SHORT).show()
-                } else {
-                    hideSoftKeyboard()
-                    val email = binding.emailEditText.text.toString()
-                    val detail = binding.detailEditText.text.toString()
-                    userRepository.postFeedback(email, detail)
-                        .bindUi(
-                            this,
-                            onSuccess = {
-                                Toast.makeText(requireContext(), "전송하였습니다", Toast.LENGTH_SHORT)
-                                    .show()
-                                findNavController().popBackStack()
-                            },
-                            onError = {
-                                apiOnError(it)
-                            }
-                        )
+                    return@bindUi
                 }
+
+                if (binding.emailEditText.text.toString().isEmpty()) {
+                    Toast.makeText(requireContext(), "이메일을 입력해 주세요", Toast.LENGTH_SHORT).show()
+                    return@bindUi
+                }
+
+                hideSoftKeyboard()
+                val email = binding.emailEditText.text.toString()
+                val detail = binding.detailEditText.text.toString()
+                userRepository.postFeedback(email, detail)
+                    .bindUi(
+                        this,
+                        onSuccess = {
+                            Toast.makeText(requireContext(), "전송하였습니다", Toast.LENGTH_SHORT)
+                                .show()
+                            findNavController().popBackStack()
+                        },
+                        onError = {
+                            apiOnError(it)
+                        }
+                    )
             }
     }
 
