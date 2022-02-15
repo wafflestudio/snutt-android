f562c7e2e26e882c263f11dab3e879e1adcefebf
sanggggg <37951125+sanggggg@users.noreply.github.com>
2022-02-15 23:08:32 +0900

    강의평 버그 수정 (SNUTT-348) (#41)
    * fix bug
    
    * touch to main
    
    * checkstyle

--- app/src/main/java/com/wafflestudio/snutt2/lib/android/HomePagerController.kt
+++ app/src/main/java/com/wafflestudio/snutt2/lib/android/HomePagerController.kt
@@ -1,28 +1,29 @@
 package com.wafflestudio.snutt2.lib.android
 
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.subjects.BehaviorSubject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class HomePagerController @Inject constructor() {
 
-    private val _homePageStream = BehaviorSubject.create<HomePage>()
-    val homePageState: Observable<HomePage> = _homePageStream.hide()
+    private val _homePageStream = MutableStateFlow(HomePage.Timetable)
+    val homePageState: StateFlow<HomePage> = _homePageStream
 
-    fun updateHomePage(page: HomePage) {
-        _homePageStream.onNext(page)
+    fun update(updater: (HomePage) -> HomePage) {
+        _homePageStream.update(updater)
+    }
+
+    fun update(page: HomePage) {
+        _homePageStream.value = page
     }
 }
 
-sealed class HomePage {
-
-    object Timetable : HomePage()
-
-    object Search : HomePage()
-
-    data class Review(val landingUrl: String) : HomePage()
-
-    object Setting : HomePage()
+enum class HomePage(val pageNum: Int) {
+    Timetable(0),
+    Search(1),
+    Review(2),
+    Setting(3),
 }
---
+++ app/src/main/java/com/wafflestudio/snutt2/lib/android/ReviewUrlController.kt
@@ -0,0 +1,22 @@
+package com.wafflestudio.snutt2.lib.android
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ReviewUrlController @Inject constructor() {
+
+    private val _urlEvent: MutableStateFlow<String?> = MutableStateFlow(null)
+    val urlEvent: StateFlow<String?> = _urlEvent
+
+    // TODO: 단 한번 emit 된 옵저버만 불리는 형태로 변경 필요
+    fun flushAfterObserve() {
+        _urlEvent.value = null
+    }
+
+    fun update(url: String) {
+        _urlEvent.value = url
+    }
+}
