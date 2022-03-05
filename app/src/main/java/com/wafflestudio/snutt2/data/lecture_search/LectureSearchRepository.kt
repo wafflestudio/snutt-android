b2344c03cb1904ca614024a88f5abdd1fd2cffcf
sanggggg <37951125+sanggggg@users.noreply.github.com>
2022-03-05 20:57:39 +0900

    move tag search to lecture search repository

--- app/src/main/java/com/wafflestudio/snutt2/data/lecture_search/LectureSearchRepository.kt
+++ app/src/main/java/com/wafflestudio/snutt2/data/lecture_search/LectureSearchRepository.kt
@@ -1,16 +1,18 @@
-package com.wafflestudio.snutt2.data.lectures
+package com.wafflestudio.snutt2.data.lecture_search
 
 import androidx.paging.PagingData
 import com.wafflestudio.snutt2.lib.network.dto.core.LectureDto
 import com.wafflestudio.snutt2.model.TagDto
 import kotlinx.coroutines.flow.Flow
 
-interface LectureRepository {
-    suspend fun getLectureResultStream(
+interface LectureSearchRepository {
+    suspend fun getLectureSearchResultStream(
         year: Long,
         semester: Long,
         title: String,
         tags: List<TagDto>,
         lecturesMask: List<Int>?
     ): Flow<PagingData<LectureDto>>
+
+    suspend fun getSearchTags(year: Long, semester: Long): List<TagDto>
 }
--- app/src/main/java/com/wafflestudio/snutt2/di/RepositoryModule.kt
+++ app/src/main/java/com/wafflestudio/snutt2/di/RepositoryModule.kt
@@ -4,14 +4,12 @@ import com.wafflestudio.snutt2.data.course_books.CourseBookRepository
 import com.wafflestudio.snutt2.data.course_books.CourseBookRepositoryImpl
 import com.wafflestudio.snutt2.data.current_table.CurrentTableRepository
 import com.wafflestudio.snutt2.data.current_table.CurrentTableRepositoryImpl
-import com.wafflestudio.snutt2.data.lectures.LectureRepository
-import com.wafflestudio.snutt2.data.lectures.LectureRepositoryImpl
+import com.wafflestudio.snutt2.data.lecture_search.LectureSearchRepository
+import com.wafflestudio.snutt2.data.lecture_search.LectureSearchRepositoryImpl
 import com.wafflestudio.snutt2.data.notifications.NotificationRepository
 import com.wafflestudio.snutt2.data.notifications.NotificationRepositoryImpl
 import com.wafflestudio.snutt2.data.tables.TableRepository
 import com.wafflestudio.snutt2.data.tables.TableRepositoryImpl
-import com.wafflestudio.snutt2.data.search_tags.SearchTagRepository
-import com.wafflestudio.snutt2.data.search_tags.SearchTagRepositoryImpl
 import com.wafflestudio.snutt2.data.user.UserRepository
 import com.wafflestudio.snutt2.data.user.UserRepositoryImpl
 import dagger.Binds
@@ -30,16 +28,13 @@ abstract class RepositoryModule {
     abstract fun bindsCurrentTableRepository(impl: CurrentTableRepositoryImpl): CurrentTableRepository
 
     @Binds
-    abstract fun bindsLectureRepository(impl: LectureRepositoryImpl): LectureRepository
+    abstract fun bindsLectureRepository(impl: LectureSearchRepositoryImpl): LectureSearchRepository
 
     @Binds
     abstract fun bindsNotificationRepository(impl: NotificationRepositoryImpl): NotificationRepository
 
     @Binds
     abstract fun bindsTableRepository(impl: TableRepositoryImpl): TableRepository
-
-    @Binds
-    abstract fun bindsSearchTagRepository(impl: SearchTagRepositoryImpl): SearchTagRepository
 
     @Binds
     abstract fun bindsUserRepository(impl: UserRepositoryImpl): UserRepository
