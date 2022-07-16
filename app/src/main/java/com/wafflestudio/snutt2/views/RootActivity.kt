3d78ef592f70fe442264e744424f37e06eb41995
sanggggg <jacob@vcnc.co.kr>
2022-07-16 18:12:23 +0900

    defautl page structure

--- app/src/main/java/com/wafflestudio/snutt2/views/RootActivity.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/RootActivity.kt
@@ -1,12 +1,25 @@
 package com.wafflestudio.snutt2.views
 
 import android.os.Bundle
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.wafflestudio.snutt2.R
 import com.wafflestudio.snutt2.data.SNUTTStorage
 import com.wafflestudio.snutt2.lib.base.BaseActivity
 import com.wafflestudio.snutt2.provider.TimetableWidgetProvider
+import com.wafflestudio.snutt2.views.logged_in.home.HomePage
 import com.wafflestudio.snutt2.views.logged_in.home.popups.PopupState
+import com.wafflestudio.snutt2.views.logged_in.lecture_detail.LectureDetailPage
+import com.wafflestudio.snutt2.views.logged_in.notifications.NotificationPage
+import com.wafflestudio.snutt2.views.logged_in.table_lectures.LecturesOfTablePage
+import com.wafflestudio.snutt2.views.logged_out.SignInPage
+import com.wafflestudio.snutt2.views.logged_out.SignUpPage
+import com.wafflestudio.snutt2.views.logged_out.TutorialPage
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -23,24 +36,52 @@ class RootActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
 
-        popupState.refreshPopupState()
 
-        val navController =
-            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
-        val navGraph = navController.navInflater.inflate(R.navigation.root_graph)
-        navGraph.setStartDestination(
-            if (snuttStorage.accessToken.get().isEmpty()) R.id.tutorialFragment
-            else R.id.homeFragment
-        )
+        findViewById<ComposeView>(R.id.compose_root).setContent {
+            setUpUI()
+        }
+    }
 
-        navController.graph = navGraph
+    @Composable
+    fun setUpUI() {
+        val navController = rememberNavController()
 
-        snuttStorage.accessToken.asObservable()
-            .distinctUntilChanged()
-            .filter { it.isEmpty() }
-            .bindUi(this) {
-                TimetableWidgetProvider.refreshWidget(this)
-                navController.navigate(R.id.startTutorial)
+        NavHost(navController = navController, startDestination = "tutorial") {
+            composable("tutorial") {
+                TutorialPage(
+                    onClickSignIn = { navController.navigate("sign_in") },
+                    onClickSignUp = { navController.navigate("sign_up") }
+                )
             }
+            composable("sign_in") {
+                SignInPage(
+                    onClickSignIn = { _, _ -> navController.navigate("home") },
+                    onClickFacebookSignIn = { navController.navigate("home") }
+                )
+            }
+            composable("sign_up") {
+                SignUpPage(
+                    onClickSignUp = { _, _, _, _ -> navController.navigate("home") },
+                    onClickFacebookSignUp = { navController.navigate("home") }
+                )
+            }
+
+            composable("home") {
+                HomePage()
+            }
+
+            composable("notification") {
+                NotificationPage()
+            }
+
+            composable("lectures_of_table") {
+                LecturesOfTablePage()
+            }
+
+            composable("lectures/{lecture_id}") {
+                val id = it.arguments?.getString("lecture_id")
+                LectureDetailPage(id = id)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/HomePage.kt
@@ -0,0 +1,17 @@
+package com.wafflestudio.snutt2.views.logged_in.home
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun HomePage() {
+    Text(text = "HomePage")
+}
+
+@Preview
+@Composable
+fun HomePagePreview() {
+    HomePage()
+}
