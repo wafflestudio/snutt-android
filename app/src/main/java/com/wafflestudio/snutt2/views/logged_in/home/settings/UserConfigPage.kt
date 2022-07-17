0acd3202b0fda3d1ea84404095cc3949d97cb5eb
sanggggg <37951125+sanggggg@users.noreply.github.com>
2022-07-17 15:16:52 +0900

    Jetpack Compose 페이지별 Navigation 스켈레톤 잡기 (#60)
    * defautl page structure
    
    * default page structure for detail pages
    
    * trimming code

---
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/UserConfigPage.kt
@@ -0,0 +1,16 @@
+package com.wafflestudio.snutt2.views.logged_in.home.settings
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun UserConfigPage() {
+    Text(text = "UserConfig")
+}
+
+@Preview
+@Composable
+fun UserConfigPagePreview() {
+    UserConfigPage()
+}
