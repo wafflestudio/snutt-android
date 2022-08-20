3353afa56ea49afdc6956ca3e3b0323ed52aa9e6
JuTaK97 <didwngus01@snu.ac.kr>
2022-08-20 17:36:09 +0900

    HomeDrawer 내부 UI 구현 (#71)
    * code style
    
    * accompanist-pager 적용
    
    * 시간표 제목 바꾸기
    
    * custom picker 컴포저블
    
    * custom dialog 컴포저블 수정
    
    * HomeDrawer 내부 UI 추가

--- app/src/main/java/com/wafflestudio/snutt2/components/compose/ProgressDialog.kt
+++ app/src/main/java/com/wafflestudio/snutt2/components/compose/ProgressDialog.kt
@@ -3,14 +3,18 @@ package com.wafflestudio.snutt2.components.compose
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.wafflestudio.snutt2.R
 
 @Composable
 fun ProgressDialog(
@@ -22,10 +26,12 @@ fun ProgressDialog(
         onDismissRequest = onDismissRequest,
         properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false)
     ) {
-        Box(modifier = Modifier
-            .background(Color.White)
-            .fillMaxWidth(0.98f)
-            .padding(20.dp)) {
+        Box(
+            modifier = Modifier
+                .background(Color.White)
+                .fillMaxWidth(0.98f)
+                .padding(20.dp)
+        ) {
             Column {
                 Text(title)
                 Spacer(modifier = Modifier.height(20.dp))
@@ -33,6 +39,42 @@ fun ProgressDialog(
                     CircularProgressIndicator()
                     Spacer(modifier = Modifier.width(30.dp))
                     Text(message)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomDialog(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+    title: String,
+    positiveButtonText: String = stringResource(R.string.common_ok),
+    negativeButtonText: String = stringResource(R.string.common_cancel),
+    content: @Composable () -> Unit
+) {
+    Dialog(onDismissRequest = onDismiss) {
+        Surface {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Row {
+                    Box(modifier = Modifier.padding(20.dp)) {
+                        Text(text = title, fontSize = 18.sp)
+                    }
+                    Box(modifier = Modifier.weight(1f))
+                }
+
+                content()
+
+                Row(modifier = Modifier.padding(vertical = 20.dp, horizontal = 30.dp)) {
+                    Box(modifier = Modifier.weight(1f))
+                    Box(modifier = Modifier.clicks { onDismiss() }) {
+                        Text(text = negativeButtonText, fontSize = 14.sp)
+                    }
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Box(modifier = Modifier.clicks { onConfirm() }) {
+                        Text(text = positiveButtonText, fontSize = 14.sp)
+                    }
                 }
             }
         }
