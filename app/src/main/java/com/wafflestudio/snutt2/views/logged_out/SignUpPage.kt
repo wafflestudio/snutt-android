e7f6d8e0ae13c568f574134d561bb7386a13f8f3
sanggggg <37951125+sanggggg@users.noreply.github.com>
2022-08-28 21:34:50 +0900

    Compose Migration 큰 변화  (#74)
    * template: add viewmodel with compose
    
    * temp
    
    * structural change
    
    * focus handling
    
    * temp
    
    * tutorial theme
    
    * bottom nav icon
    
    * build success
    
    * fix namings
    
    * checkstyle

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_out/SignUpPage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_out/SignUpPage.kt
@@ -1,43 +1,49 @@
 package com.wafflestudio.snutt2.views.logged_out
 
-import android.widget.Toast
-import androidx.activity.result.ActivityResultRegistryOwner
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
-import com.facebook.login.LoginManager
-import com.facebook.login.LoginResult
 import com.wafflestudio.snutt2.R
-import com.wafflestudio.snutt2.components.compose.ProgressDialog
-import com.wafflestudio.snutt2.views.NavControllerContext
-import com.wafflestudio.snutt2.views.NavigationDestination
-import com.wafflestudio.snutt2.views.navigateAsOrigin
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.kotlin.subscribeBy
+import com.wafflestudio.snutt2.components.compose.BorderButton
+import com.wafflestudio.snutt2.components.compose.EditText
+import com.wafflestudio.snutt2.lib.android.toast
+import com.wafflestudio.snutt2.ui.SNUTTColors
+import com.wafflestudio.snutt2.ui.SNUTTTypography
+import com.wafflestudio.snutt2.views.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.rx3.await
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SignUpPage() {
-    val navController = NavControllerContext.current
+    val navController = LocalNavController.current
+    val context = LocalContext.current
+    val apiOnError = LocalApiOnError.current
+    val apiOnProgress = LocalApiOnProgress.current
+
+    val focusManager = LocalFocusManager.current
+    val coroutineScope = rememberCoroutineScope()
+
     val authViewModel = hiltViewModel<AuthViewModel>()
 
     var idField by remember { mutableStateOf("") }
@@ -45,18 +51,39 @@ fun SignUpPage() {
     var passwordConfirmField by remember { mutableStateOf("") }
     var emailField by remember { mutableStateOf("") }
 
-    val keyboardManager = LocalSoftwareKeyboardController.current
-    val callbackManager = CallbackManager.Factory.create()
-    val loginManager = LoginManager.getInstance()
-    val context = LocalContext.current
-
-    var showProgressDialog by remember { mutableStateOf(false) }
-    if (showProgressDialog) {
-        ProgressDialog(
-            title = stringResource(R.string.sign_up_sign_up_button),
-            message = stringResource(R.string.sign_in_progress_bar_message),
-            onDismissRequest = { showProgressDialog = false }
-        )
+    val handleLocalSignUp = {
+        val isPasswordConfirmPassed = (passwordConfirmField == passwordField)
+        if (isPasswordConfirmPassed.not()) {
+            context.toast(context.getString(R.string.sign_up_password_confirm_invalid_toast))
+        } else {
+            coroutineScope.launch {
+                try {
+                    apiOnProgress.showProgress()
+                    authViewModel.signUpLocal(idField, emailField, passwordField).await()
+                    navController.navigateAsOrigin(NavigationDestination.Home)
+                } catch (e: Exception) {
+                    apiOnError(e)
+                } finally {
+                    apiOnProgress.hideProgress()
+                }
+            }
+        }
+    }
+    val handleFacebookSignUp = {
+        coroutineScope.launch {
+            try {
+                apiOnProgress.showProgress()
+                val loginResult = facebookLogin(context)
+                val id = loginResult.accessToken.userId
+                val token = loginResult.accessToken.token
+                authViewModel.signUpFacebook(id, token).await()
+                navController.navigateAsOrigin(NavigationDestination.Home)
+            } catch (e: Exception) {
+                apiOnError(e)
+            } finally {
+                apiOnProgress.hideProgress()
+            }
+        }
     }
 
     Column(
@@ -67,14 +94,15 @@ fun SignUpPage() {
             .padding(30.dp)
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(
-                text = stringResource(R.string.sign_up_logo_title),
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = stringResource(R.string.sign_in_logo_title),
                 modifier = Modifier.padding(top = 40.dp, bottom = 15.dp),
             )
 
-            Image(
-                painter = painterResource(id = R.drawable.logo),
-                contentDescription = stringResource(R.string.sign_up_logo_title)
+            Text(
+                text = stringResource(R.string.sign_in_logo_title),
+                style = SNUTTTypography.h1,
             )
         }
 
@@ -86,138 +114,117 @@ fun SignUpPage() {
                 .padding(top = 60.dp, bottom = 20.dp)
                 .verticalScroll(rememberScrollState())
         ) {
-            TextField(
+            EditText(
                 value = idField,
                 onValueChange = { idField = it },
-                placeholder = {
-                    Text(text = stringResource(R.string.sign_up_id_hint))
-                },
+                hint = stringResource(R.string.sign_up_id_hint),
+                keyboardActions = KeyboardActions(onNext = { focusManager.moveFocus(FocusDirection.Down) }),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
                 modifier = Modifier.fillMaxWidth(),
             )
 
-            TextField(
-                value = passwordField,
-                onValueChange = { passwordField = it },
-                placeholder = {
-                    Text(text = stringResource(R.string.sign_up_password_hint))
-                },
-                modifier = Modifier.fillMaxWidth(),
-            )
-
-            TextField(
+            EditText(
                 value = emailField,
                 onValueChange = { emailField = it },
-                placeholder = {
-                    Text(text = stringResource(R.string.sign_up_email_input_hint))
-                },
+                keyboardActions = KeyboardActions(onNext = { focusManager.moveFocus(FocusDirection.Down) }),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                hint = stringResource(R.string.sign_up_email_input_hint),
                 modifier = Modifier.fillMaxWidth(),
             )
 
-            TextField(
+            EditText(
+                value = passwordField,
+                onValueChange = { passwordField = it },
+                hint = stringResource(R.string.sign_up_password_hint),
+                keyboardActions = KeyboardActions(onNext = { focusManager.moveFocus(FocusDirection.Down) }),
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next,
+                    keyboardType = KeyboardType.Password
+                ),
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth(),
+            )
+
+            EditText(
                 value = passwordConfirmField,
                 onValueChange = { passwordConfirmField = it },
-                placeholder = {
-                    Text(text = stringResource(R.string.sign_up_password_confirm_hint))
-                },
+                keyboardActions = KeyboardActions(onDone = { handleLocalSignUp() }),
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done,
+                    keyboardType = KeyboardType.Password
+                ),
+                visualTransformation = PasswordVisualTransformation(),
+                hint = stringResource(R.string.sign_up_password_confirm_hint),
                 modifier = Modifier.fillMaxWidth(),
             )
         }
 
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(10.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
             modifier = Modifier.wrapContentHeight()
         ) {
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(45.dp),
-                shape = RectangleShape,
-                onClick = { // TODO: throttling
-                    val passwordConfirmPass = (passwordConfirmField == passwordField)
-                    if (passwordConfirmPass) {
-                        keyboardManager?.hide()
-                        showProgressDialog = true
-                        authViewModel.signUpLocal(idField, emailField, passwordField)
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .subscribeBy( // TODO: dispose
-                                onSuccess = {
-                                    navController.navigateAsOrigin(NavigationDestination.Home)
-                                    showProgressDialog = false
-                                },
-                                onError = { // TODO: onError
-                                    showProgressDialog = false
-                                }
-                            )
-                    } else {
-                        Toast.makeText(
-                            context,
-                            context.getString(R.string.sign_up_password_confirm_invalid_toast),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
+            BorderButton(
+                modifier = Modifier.fillMaxWidth(),
+                color = SNUTTColors.Gray200,
+                onClick = { handleLocalSignUp() }
             ) {
-                Text(text = stringResource(R.string.sign_up_sign_up_button))
-            }
-
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(45.dp),
-                onClick = { // TODO: throttling
-                    loginManager.logInWithReadPermissions(
-                        context as ActivityResultRegistryOwner,
-                        callbackManager,
-                        emptyList()
-                    )
-                }
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.iconfacebook),
-                    contentDescription = stringResource(id = R.string.sign_up_sign_up_facebook_button),
-                    modifier = Modifier.padding(end = 12.dp)
+                Text(
+                    text = stringResource(R.string.sign_up_sign_up_button),
+                    style = SNUTTTypography.button
                 )
-                Text(text = stringResource(R.string.sign_up_sign_up_facebook_button))
-            }
-        }
-        Text(
-            text = stringResource(id = R.string.sign_up_terms),
-            modifier = Modifier.padding(top = 20.dp)
-        )
-    }
-
-    loginManager.registerCallback(
-        callbackManager,
-        object : FacebookCallback<LoginResult> {
-            override fun onCancel() {
-                Toast.makeText(
-                    context,
-                    context.getString(R.string.sign_up_facebook_login_failed_toast),
-                    Toast.LENGTH_SHORT
-                ).show()
             }
 
-            override fun onError(error: FacebookException) {
-                Toast.makeText(
-                    context,
-                    context.getString(R.string.sign_up_facebook_login_failed_toast),
-                    Toast.LENGTH_SHORT
-                ).show()
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Box(
+                    modifier = Modifier
+                        .background(SNUTTColors.Gray100)
+                        .height(1.dp)
+                        .weight(1f)
+                )
+
+                Text(
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                    text = "or", style = SNUTTTypography.body2, color = SNUTTColors.Gray200
+                )
+
+                Box(
+                    modifier = Modifier
+                        .background(SNUTTColors.Gray100)
+                        .height(1.dp)
+                        .weight(1f)
+                )
             }
 
-            override fun onSuccess(result: LoginResult) {
-                val id = result.accessToken.userId
-                val token = result.accessToken.token
-                authViewModel.signUpFacebook(id, token)
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeBy( // TODO: Dispose
-                        onError = {}, // TODO: onError
-                        onSuccess = { navController.navigateAsOrigin(NavigationDestination.Home) }
+            BorderButton(
+                modifier = Modifier.fillMaxWidth(),
+                color = SNUTTColors.FacebookBlue,
+                onClick = { handleFacebookSignUp() }
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(
+                        painter = painterResource(id = R.drawable.iconfacebook),
+                        contentDescription = stringResource(id = R.string.sign_up_sign_up_facebook_button),
+                        modifier = Modifier
+                            .height(18.dp)
+                            .padding(end = 12.dp),
                     )
+
+                    Text(
+                        text = stringResource(R.string.sign_up_sign_up_facebook_button),
+                        color = SNUTTColors.FacebookBlue,
+                        style = SNUTTTypography.button
+                    )
+                }
             }
+
+            Text(
+                text = stringResource(id = R.string.sign_up_terms),
+                modifier = Modifier.padding(top = 20.dp),
+                style = SNUTTTypography.body2
+            )
         }
-    )
+    }
 
     // TODO: 이용 약관 버튼
 }
