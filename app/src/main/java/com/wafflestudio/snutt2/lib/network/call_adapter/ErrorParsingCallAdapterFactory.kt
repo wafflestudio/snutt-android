70cce3d72bc4649e4e71d43f6d1c8607aac57cbc
sanggggg <jacob@vcnc.co.kr>
2022-09-04 02:25:57 +0900

    error parsing with kotlin suspend

--- app/src/main/java/com/wafflestudio/snutt2/lib/network/call_adapter/ErrorParsingCallAdapterFactory.kt
+++ app/src/main/java/com/wafflestudio/snutt2/lib/network/call_adapter/ErrorParsingCallAdapterFactory.kt
@@ -1,8 +1,10 @@
 package com.wafflestudio.snutt2.lib.network.call_adapter
 
 import com.wafflestudio.snutt2.lib.data.serializer.Serializer
+import retrofit2.Call
 import retrofit2.CallAdapter
 import retrofit2.Retrofit
+import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 
 class ErrorParsingCallAdapterFactory(
@@ -16,14 +18,17 @@ class ErrorParsingCallAdapterFactory(
         annotations: Array<out Annotation>,
         retrofit: Retrofit
     ): CallAdapter<*, *>? {
-        val delegationGeneratedCallAdapter =
-            delegation.get(returnType, annotations, retrofit) as? CallAdapter<Any, Any>
-
-        return delegationGeneratedCallAdapter?.let {
+        if (returnType.javaClass == Call::class.java) {
+            val delegationGeneratedCallAdapter =
+                delegation.get(returnType, annotations, retrofit) as? CallAdapter<Any, Any>
             return ErrorParsingCallAdapter(
                 serializer = serializer,
-                delegation = delegationGeneratedCallAdapter
-            )
+                delegation = delegationGeneratedCallAdapter,
+                // 당장은 Call
+                bodyType = (returnType as? ParameterizedType)?.let {
+                    it.actualTypeArguments[0]
+                } ?: Unit::class.java)
         }
+        return null
     }
 }
