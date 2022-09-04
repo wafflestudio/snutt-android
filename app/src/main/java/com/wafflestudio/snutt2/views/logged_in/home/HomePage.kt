cf2154a178c245beac8c6561d992f9b9aaef4f8d
sanggggg <37951125+sanggggg@users.noreply.github.com>
2022-09-04 17:17:55 +0900

    BottomNavigation 과 TopBar 의 상/하단 shadow 추가 (#79)

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/HomePage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/HomePage.kt
@@ -4,17 +4,27 @@ import androidx.annotation.DrawableRes
 import androidx.compose.animation.Animatable
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.DrawerValue
 import androidx.compose.material.ModalDrawer
+import androidx.compose.material.Surface
 import androidx.compose.material.rememberDrawerState
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.wafflestudio.snutt2.R
@@ -161,7 +171,10 @@ fun HomePage() {
             Column(
                 modifier = Modifier.fillMaxSize()
             ) {
-                Box(modifier = Modifier.weight(1f)) {
+                Box(
+                    modifier = Modifier.weight(1f),
+                    contentAlignment = Alignment.BottomCenter
+                ) {
                     when (pageState) {
                         HomeItem.Timetable -> TimetablePage(uncheckedNotification = uncheckedNotification)
                         HomeItem.Search -> SearchPage(
@@ -170,73 +183,94 @@ fun HomePage() {
                         HomeItem.Review -> ReviewPage()
                         HomeItem.Settings -> SettingsPage()
                     }
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(1.dp)
+                            .background(
+                                brush = Brush.verticalGradient(
+                                    listOf(
+                                        Color.Transparent,
+                                        SNUTTColors.Gray100
+                                    )
+                                ),
+                            )
+                    )
                 }
-                Row(
-                    modifier = Modifier
-                        .height(56.dp)
-                        .fillMaxWidth()
-                ) {
-                    BorderButton(
-                        color = SNUTTColors.White900,
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxHeight(),
-                        onClick = {
-                            pageState = HomeItem.Timetable
-                        },
-                    ) {
-                        TimetableIcon(
-                            modifier = Modifier.size(30.dp),
-                            isSelected = pageState == HomeItem.Timetable
-                        )
-                    }
 
-                    BorderButton(
-                        color = SNUTTColors.White900,
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxHeight(),
-                        onClick = {
-                            pageState = HomeItem.Search
-                        },
-                    ) {
-                        SearchIcon(
-                            modifier = Modifier.size(30.dp),
-                            isSelected = pageState == HomeItem.Search
-                        )
-                    }
-
-                    BorderButton(
-                        color = SNUTTColors.White900,
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxHeight(),
-                        onClick = {
-                            pageState = HomeItem.Review
-                        },
-                    ) {
-                        ReviewIcon(
-                            modifier = Modifier.size(30.dp),
-                            isSelected = pageState == HomeItem.Review
-                        )
-                    }
-
-                    BorderButton(
-                        color = SNUTTColors.White900,
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxHeight(),
-                        onClick = {
-                            pageState = HomeItem.Settings
-                        },
-                    ) {
-                        SettingIcon(
-                            modifier = Modifier.size(30.dp),
-                            isSelected = pageState == HomeItem.Settings
-                        )
-                    }
-                }
+                BottomNavigation(pageState = pageState, onUpdatePageState = { pageState = it })
             }
+        }
+    }
+}
+
+@Composable
+private fun BottomNavigation(pageState: HomeItem, onUpdatePageState: (HomeItem) -> Unit) {
+    Row(
+        modifier = Modifier
+            .height(56.dp)
+            .fillMaxWidth()
+            .background(SNUTTColors.White900)
+    ) {
+        BorderButton(
+            color = SNUTTColors.White900,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            onClick = {
+                onUpdatePageState(HomeItem.Timetable)
+            },
+        ) {
+            TimetableIcon(
+                modifier = Modifier.size(30.dp),
+                isSelected = pageState == HomeItem.Timetable
+            )
+        }
+
+        BorderButton(
+            color = SNUTTColors.White900,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            onClick = {
+                onUpdatePageState(HomeItem.Search)
+            },
+        ) {
+            SearchIcon(
+                modifier = Modifier.size(30.dp),
+                isSelected = pageState == HomeItem.Search
+            )
+        }
+
+        BorderButton(
+            color = SNUTTColors.White900,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            onClick = {
+                onUpdatePageState(HomeItem.Review)
+            },
+        ) {
+            ReviewIcon(
+                modifier = Modifier.size(30.dp),
+                isSelected = pageState == HomeItem.Review
+            )
+        }
+
+        BorderButton(
+            color = SNUTTColors.White900,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            onClick = {
+                onUpdatePageState(HomeItem.Settings)
+            },
+        ) {
+            SettingIcon(
+                modifier = Modifier.size(30.dp),
+                isSelected = pageState == HomeItem.Settings
+            )
         }
     }
 }
