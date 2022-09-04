12450cfa602ac7878b64c4cc9d05e022f7ef1b04
sanggggg <37951125+sanggggg@users.noreply.github.com>
2022-09-04 13:39:29 +0900

    kotlin suspend fun 사용시에도 error dto 를 파싱해서 돌려주도록 변경 (#77)
    * error parsing with kotlin suspend
    
    * handle all types

--- app/src/main/java/com/wafflestudio/snutt2/lib/network/call_adapter/ErrorParsingCallAdapterFactory.kt
+++ app/src/main/java/com/wafflestudio/snutt2/lib/network/call_adapter/ErrorParsingCallAdapterFactory.kt
@@ -1,8 +1,11 @@
 package com.wafflestudio.snutt2.lib.network.call_adapter
 
+import android.util.Log
 import com.wafflestudio.snutt2.lib.data.serializer.Serializer
+import retrofit2.Call
 import retrofit2.CallAdapter
 import retrofit2.Retrofit
+import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 
 class ErrorParsingCallAdapterFactory(
@@ -15,15 +18,14 @@ class ErrorParsingCallAdapterFactory(
         returnType: Type,
         annotations: Array<out Annotation>,
         retrofit: Retrofit
-    ): CallAdapter<*, *>? {
+    ): CallAdapter<*, *> {
         val delegationGeneratedCallAdapter =
             delegation.get(returnType, annotations, retrofit) as? CallAdapter<Any, Any>
-
-        return delegationGeneratedCallAdapter?.let {
-            return ErrorParsingCallAdapter(
-                serializer = serializer,
-                delegation = delegationGeneratedCallAdapter
-            )
-        }
+        return ErrorParsingCallAdapter(
+            serializer = serializer,
+            delegation = delegationGeneratedCallAdapter,
+            bodyType = (returnType as? ParameterizedType)?.let {
+                it.actualTypeArguments[0]
+            } ?: Unit::class.java)
     }
 }
