261bf70b63484b3fff9706d812bb5578e146c605
양주현 <didwngus01@snu.ac.kr>
2022-10-01 16:47:35 +0900

    HomeDrawer 내부 프레임 드랍 해결 (#84)
    * HomeDrawer 내부 프레임 드랍 해결
    
    * check style

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/TableListViewModelNew.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/TableListViewModelNew.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 package com.wafflestudio.snutt2.views.logged_in.home
 
 import androidx.lifecycle.ViewModel
@@ -7,6 +9,7 @@ import com.wafflestudio.snutt2.data.current_table.CurrentTableRepository
 import com.wafflestudio.snutt2.data.tables.TableRepository
 import com.wafflestudio.snutt2.lib.network.dto.core.CourseBookDto
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
@@ -19,18 +22,24 @@ class TableListViewModelNew @Inject constructor(
 
     private val _allCourseBook = MutableStateFlow(emptyList<CourseBookDto>())
     val allCourseBook = _allCourseBook.asStateFlow()
-    val mostRecentCourseBook = _allCourseBook.filter { it.isNotEmpty() }.map { it.first() }
+    private val mostRecentCourseBook = _allCourseBook.filter { it.isNotEmpty() }.map { it.first() }
 
-    val tableListOfEachCourseBook = tableRepository.tableMap.stateIn(
+    private val tableMap = tableRepository.tableMap.stateIn(
         viewModelScope, SharingStarted.WhileSubscribed(), initialValue = mapOf()
     )
 
-    val courseBooksWithTable = tableListOfEachCourseBook.map {
-        (
-            it.values.map { table ->
-                CourseBookDto(year = table.year, semester = table.semester)
-            } + mostRecentCourseBook.first()
-            ).distinct()
+    val courseBooksWhichHaveTable = tableMap.map {
+        (it.values.map { table ->
+            CourseBookDto(year = table.year, semester = table.semester)
+        } + mostRecentCourseBook.first()).distinct().sorted()
+    }
+
+    val tableListOfEachCourseBook = courseBooksWhichHaveTable.map {
+        it.associateWith { courseBook ->
+            tableMap.value.values.filter { table ->
+                table.year == courseBook.year && table.semester == courseBook.semester
+            }
+        }
     }
 
     val newSemesterNotify = tableRepository.tableMap.map { tableMap ->
@@ -61,9 +70,7 @@ class TableListViewModelNew @Inject constructor(
 
     suspend fun createTableNew(courseBook: CourseBookDto, tableName: String) {
         tableRepository.createTable(
-            year = courseBook.year,
-            semester = courseBook.semester,
-            title = tableName
+            year = courseBook.year, semester = courseBook.semester, title = tableName
         )
     }
 
