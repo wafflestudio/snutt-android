1621032d01d13ec0d33683aa665e589d099736dc
JuTaK97 <didwngus01@snu.ac.kr>
2022-10-23 02:08:24 +0900

    다이얼로그 title 폰트 어색한 부분 수정

--- app/src/main/java/com/wafflestudio/snutt2/components/compose/ProgressDialog.kt
+++ app/src/main/java/com/wafflestudio/snutt2/components/compose/ProgressDialog.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -61,7 +62,7 @@ fun CustomDialog(
                 title?.let {
                     Row {
                         Box(modifier = Modifier.padding(20.dp)) {
-                            Text(text = it, style = SNUTTTypography.h2)
+                            Text(text = it, style = SNUTTTypography.h2.copy(fontWeight = FontWeight.Normal))
                         }
                         Box(modifier = Modifier.weight(1f))
                     }
