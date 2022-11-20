d70114da39f43720d45eae6be11f1216cb80cfeb
JuTaK97 <didwngus01@snu.ac.kr>
2022-11-20 14:00:11 +0900

    다크모드 색, 테마 추가

--- app/src/main/java/com/wafflestudio/snutt2/ui/Theme.kt
+++ app/src/main/java/com/wafflestudio/snutt2/ui/Theme.kt
@@ -2,10 +2,22 @@ package com.wafflestudio.snutt2.ui
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val LightThemeColors = lightColors(
+private val LightThemeColors @Composable get() = lightColors(
+    primary = SNUTTColors.White900,
+    primaryVariant = SNUTTColors.Gray400,
+    onPrimary = SNUTTColors.Black900,
+    error = SNUTTColors.Red,
+    background = SNUTTColors.White900,
+    onBackground = SNUTTColors.Black900,
+    surface = SNUTTColors.White900,
+    onSurface = SNUTTColors.Black900,
+)
+
+private val DarkThemeColors @Composable get() = darkColors(
     primary = SNUTTColors.White900,
     primaryVariant = SNUTTColors.Gray400,
     onPrimary = SNUTTColors.Black900,
@@ -22,7 +34,7 @@ fun SNUTTTheme(
     content: @Composable () -> Unit
 ) {
     MaterialTheme(
-        colors = LightThemeColors,
+        colors = if (darkTheme) DarkThemeColors else LightThemeColors,
         typography = SNUTTTypography,
         content = content
     )
