3aa167ac57261de835b33d3d211f0fcbf9ec02c4
JuTaK97 <didwngus01@snu.ac.kr>
2022-12-20 22:36:36 +0900

    lint

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/AppReportPage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/AppReportPage.kt
@@ -74,7 +74,6 @@ fun AppReportPage() {
                                     navController.popBackStack()
                                 }
                             }
-
                         }
                     }
             )
