1730fea3990a756bb6819fbc1ac76d63c80301d9
양주현 <didwngus01@snu.ac.kr>
2022-12-26 18:13:07 +0900

    시간-교시 변경 구 클라 대응 (#107)

--- app/src/main/java/com/wafflestudio/snutt2/lib/network/dto/core/ClassTimeDto.kt
+++ app/src/main/java/com/wafflestudio/snutt2/lib/network/dto/core/ClassTimeDto.kt
@@ -4,6 +4,7 @@ import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
+import kotlin.math.roundToInt
 
 @JsonClass(generateAdapter = true)
 @Parcelize
@@ -13,29 +14,29 @@ data class ClassTimeDto(
     @Json(name = "_id") val id: String? = null,
     @Json(name = "start_time") val start_time: String = "",
     @Json(name = "end_time") val end_time: String = "",
-    @Json(name = "start") val start: Float, // old
-    @Json(name = "len") val len: Float, // old
+    @Json(name = "start") val start: Float, // deprecated
+    @Json(name = "len") val len: Float, // deprecated
 ) : Parcelable {
 
     val startTimeInFloat: Float
         get() =
             if (start_time.isNotEmpty()) start_time.split(':')[0].toFloat() + start_time.split(':')[1].toFloat() / 60f
-            else start
+            else start + 8 // 구 클라 대응
 
     val endTimeInFloat: Float
         get() =
             if (end_time.isNotEmpty()) end_time.split(':')[0].toFloat() + end_time.split(':')[1].toFloat() / 60f
-            else start + len
+            else start + len + 8 // 구 클라 대응
 
     val startTimeHour: Int
         get() = startTimeInFloat.toInt()
 
     val startTimeMinute: Int
-        get() = start_time.split(':')[1].toInt()
+        get() = (60 * (startTimeInFloat - startTimeHour)).roundToInt()
 
     val endTimeHour: Int
         get() = endTimeInFloat.toInt()
 
     val endTimeMinute: Int
-        get() = end_time.split(':')[1].toInt()
+        get() = (60 * (endTimeInFloat - endTimeHour)).roundToInt()
 }
