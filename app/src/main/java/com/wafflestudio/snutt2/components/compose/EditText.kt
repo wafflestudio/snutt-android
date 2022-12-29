3a34634d00c60efc673ff34158de836fa2bc0d15
양주현 <didwngus01@snu.ac.kr>
2022-12-29 15:39:45 +0900

    TextField의 커서 핸들 색이 라이트모드에서도 흰색이라 어색한 부분 수정 (#110)

--- app/src/main/java/com/wafflestudio/snutt2/components/compose/EditText.kt
+++ app/src/main/java/com/wafflestudio/snutt2/components/compose/EditText.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -45,48 +47,55 @@ fun EditText(
     }
 
     var isFocused by remember { mutableStateOf(false) }
+    val customTextSelectionColors = TextSelectionColors(
+        handleColor = SNUTTColors.Black900,
+        backgroundColor = SNUTTColors.Black900
+    )
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides customTextSelectionColors,
+    ) {
+        BasicTextField(
+            modifier = modifier
+                .onFocusChanged { isFocused = it.isFocused },
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            value = value,
+            textStyle = textStyle,
+            enabled = enabled,
+            onValueChange = onValueChange,
+            singleLine = singleLine,
+            visualTransformation = visualTransformation,
+            decorationBox = {
 
-    BasicTextField(
-        modifier = modifier
-            .onFocusChanged { isFocused = it.isFocused },
-        keyboardOptions = keyboardOptions,
-        keyboardActions = keyboardActions,
-        value = value,
-        textStyle = textStyle,
-        enabled = enabled,
-        onValueChange = onValueChange,
-        singleLine = singleLine,
-        visualTransformation = visualTransformation,
-        decorationBox = {
-
-            Column {
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    leadingIcon()
-                    if (value.isNotEmpty() || isFocused) { // FIXME: lectureDetail 에서는 focus 되어 있어도 empty이면 hint 가 나와야 한다.
-                        it()
-                    } else {
-                        hint?.let {
-                            Text(
-                                text = it,
-                                style = textStyle.copy(color = SNUTTColors.Gray200),
-                            )
+                Column {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        leadingIcon()
+                        if (value.isNotEmpty() || isFocused) { // FIXME: lectureDetail 에서는 focus 되어 있어도 empty이면 hint 가 나와야 한다.
+                            it()
+                        } else {
+                            hint?.let {
+                                Text(
+                                    text = it,
+                                    style = textStyle.copy(color = SNUTTColors.Gray200),
+                                )
+                            }
                         }
+                        trailingIcon()
                     }
-                    trailingIcon()
-                }
 
-                if (underlineEnabled) {
-                    Box(
-                        modifier = Modifier
-                            .padding(top = 8.dp)
-                            .background(if (isFocused) underlineColorFocused else underlineColor)
-                            .fillMaxWidth()
-                            .height(underlineWidth)
-                    )
+                    if (underlineEnabled) {
+                        Box(
+                            modifier = Modifier
+                                .padding(top = 8.dp)
+                                .background(if (isFocused) underlineColorFocused else underlineColor)
+                                .fillMaxWidth()
+                                .height(underlineWidth)
+                        )
+                    }
                 }
             }
-        }
-    )
+        )
+    }
 }
 
 @Preview(showBackground = true)
