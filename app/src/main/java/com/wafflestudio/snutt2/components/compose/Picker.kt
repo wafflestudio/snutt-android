81c117749eb2dc2cf84b718a5161e9e7e5f4f317
JuTaK97 <didwngus01@snu.ac.kr>
2022-12-29 23:17:37 +0900

    Picker 좀 더 자연스럽게 바꾸기

--- app/src/main/java/com/wafflestudio/snutt2/components/compose/Picker.kt
+++ app/src/main/java/com/wafflestudio/snutt2/components/compose/Picker.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.wafflestudio.snutt2.ui.SNUTTColors
@@ -22,18 +23,18 @@ fun <T> Picker(
     list: List<T>,
     initialCenterIndex: Int,
     onValueChanged: (Int) -> Unit,
+    columnHeightDp: Dp = 35.dp,
     PickerItemContent: @Composable (Int) -> Unit,
 ) {
     val scope = rememberCoroutineScope()
-    val columnHeightDp = 35.dp
 
     val animatedOffset = remember { Animatable(initialCenterIndex * columnHeightDp.value) }
         .apply {
-            updateBounds(0f, (columnHeightDp.value * list.size))
+            updateBounds(columnHeightDp.value * (- 0.5f), columnHeightDp.value * (list.size - 0.5f))
         }
     // 중앙에 두 item의 중간이 위치할 때 threshold 및 centerItemIndex 값이 변경
     val threshold = (animatedOffset.value + columnHeightDp.value / 2) % columnHeightDp.value
-    var centerItemIndex by remember(threshold) {
+    val centerItemIndex by remember(threshold) {
         mutableStateOf(
             (animatedOffset.value / columnHeightDp.value).roundToInt().coerceAtMost(list.size - 1)
         )
@@ -46,13 +47,6 @@ fun <T> Picker(
     val localOffset =
         if (animatedOffset.value % columnHeightDp.value > columnHeightDp.value / 2) (columnHeightDp.value - (animatedOffset.value % columnHeightDp.value))
         else -(animatedOffset.value % columnHeightDp.value)
-
-    // list 자체의 변경에 따른 변화를 반영한다. (recompose 로 되는 방법 찾기)
-    // FIXME: 시작 시간의 변경에 따라 끝나는 시간의 list 가 바뀌는데, 이때 미세한 떨림 존재
-    LaunchedEffect(list.size) {
-        centerItemIndex = initialCenterIndex
-        animatedOffset.snapTo(columnHeightDp.value * centerItemIndex)
-    }
 
     Box(
         contentAlignment = Alignment.Center,
