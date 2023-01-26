a897993afcdc0785e230cf550aef2c647e263c20
양주현 <didwngus01@snu.ac.kr>
2023-01-26 23:25:45 +0900

    커스텀 강좌 학점 보이지 않는 문제 핫픽스 (#137)

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/lecture_detail/LectureDetailPage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/lecture_detail/LectureDetailPage.kt
@@ -89,6 +89,7 @@ fun LectureDetailPage(onCloseViewMode: () -> Unit = {}) {
     var deleteTimeDialogState by remember { mutableStateOf(false) }
     var lectureOverlapDialogState by remember { mutableStateOf(false) }
     var lectureOverlapDialogMessage by remember { mutableStateOf("") }
+    var creditText by remember { mutableStateOf(editingLectureDetail.credit.toString()) }
 
     /* 바텀시트 관련 */
     val sheetState = rememberModalBottomSheetState(
@@ -335,20 +336,23 @@ fun LectureDetailPage(onCloseViewMode: () -> Unit = {}) {
                                     hint = stringResource(R.string.lecture_detail_hint_nothing),
                                 )
                             }
-                            LectureDetailItem(title = stringResource(R.string.lecture_detail_credit)) {
-                                EditText(
-                                    value = editingLectureDetail.credit.toString(),
-                                    onValueChange = {
-                                        vm.editEditingLectureDetail(editingLectureDetail.copy(credit = it.stringToLong()))
-                                    },
-                                    enabled = editMode,
-                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                                    modifier = Modifier.fillMaxWidth(),
-                                    underlineEnabled = false,
-                                    textStyle = SNUTTTypography.body1.copy(fontSize = 15.sp),
-                                    hint = "0",
-                                )
-                            }
+                        }
+                        LectureDetailItem(title = stringResource(R.string.lecture_detail_credit)) {
+                            EditText(
+                                value = creditText,
+                                onValueChange = {
+                                    creditText = it
+                                    vm.editEditingLectureDetail(editingLectureDetail.copy(credit = it.stringToLong()))
+                                },
+                                enabled = editMode,
+                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                                modifier = Modifier.fillMaxWidth(),
+                                underlineEnabled = false,
+                                textStyle = SNUTTTypography.body1.copy(fontSize = 15.sp),
+                                hint = "0",
+                            )
+                        }
+                        if (isCustom.not()) {
                             LectureDetailItem(title = stringResource(R.string.lecture_detail_classification)) {
                                 EditText(
                                     value = editingLectureDetail.classification ?: "",
