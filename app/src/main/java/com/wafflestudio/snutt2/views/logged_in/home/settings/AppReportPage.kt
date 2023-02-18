d23d00e2cb99372ee2dcf4f94464012850fa3740
JuTaK97 <didwngus01@snu.ac.kr>
2023-02-18 15:10:11 +0900

    개발자 괴롭히기 화면 개선
    - keyboardOptions, keyboardActions 추가

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/AppReportPage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/AppReportPage.kt
@@ -2,12 +2,20 @@ package com.wafflestudio.snutt2.views.logged_in.home.settings
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,17 +32,37 @@ import com.wafflestudio.snutt2.views.LocalNavController
 import com.wafflestudio.snutt2.views.launchSuspendApi
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun AppReportPage() {
     val context = LocalContext.current
     val navController = LocalNavController.current
     val apiOnError = LocalApiOnError.current
     val apiOnProgress = LocalApiOnProgress.current
+    val keyboardManager = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
     val scope = rememberCoroutineScope()
     val userViewModel = hiltViewModel<UserViewModel>()
 
     var email by remember { mutableStateOf("") }
     var detail by remember { mutableStateOf("") }
+
+    val sendFeedback = {
+        if (detail.isEmpty()) {
+            context.toast(context.getString(R.string.feedback_empty_detail_warning))
+        } else if (email.isEmailInvalid()) {
+            context.toast(context.getString(R.string.feedback_invalid_email_warning))
+        } else {
+            scope.launch {
+                launchSuspendApi(apiOnProgress, apiOnError) {
+                    userViewModel.sendFeedback(email, detail)
+                    keyboardManager?.hide()
+                    context.toast(context.getString(R.string.feedback_send_success_message))
+                    navController.popBackStack()
+                }
+            }
+        }
+    }
 
     Column(
         modifier = Modifier
@@ -59,21 +87,7 @@ fun AppReportPage() {
             SendIcon(
                 modifier = Modifier
                     .padding(6.dp)
-                    .clicks {
-                        if (detail.isEmpty()) {
-                            context.toast(context.getString(R.string.feedback_empty_detail_warning))
-                        } else if (email.isEmailInvalid()) {
-                            context.toast(context.getString(R.string.feedback_invalid_email_warning))
-                        } else {
-                            scope.launch {
-                                launchSuspendApi(apiOnProgress, apiOnError) {
-                                    userViewModel.sendFeedback(email, detail)
-                                    context.toast(context.getString(R.string.feedback_send_success_message))
-                                    navController.popBackStack()
-                                }
-                            }
-                        }
-                    }
+                    .clicks { sendFeedback() }
             )
         })
         Column(modifier = Modifier.padding(horizontal = 20.dp)) {
@@ -88,6 +102,15 @@ fun AppReportPage() {
                 onValueChange = { email = it },
                 hint = "example@gmail.com",
                 textStyle = SNUTTTypography.body1.copy(fontSize = 17.sp),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next, keyboardType = KeyboardType.Email
+                ),
+                keyboardActions = KeyboardActions(onNext = {
+                    focusManager.moveFocus(
+                        FocusDirection.Down
+                    )
+                }),
             )
             Spacer(modifier = Modifier.height(20.dp))
             Text(
@@ -99,6 +122,10 @@ fun AppReportPage() {
                 value = detail, onValueChange = { detail = it },
                 hint = "불편한 점이나 버그를 적어주세요",
                 textStyle = SNUTTTypography.body1.copy(fontSize = 17.sp),
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done, keyboardType = KeyboardType.Ascii,
+                ),
+                keyboardActions = KeyboardActions(onDone = { sendFeedback() }),
             )
         }
     }
