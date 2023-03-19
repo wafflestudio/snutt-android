3b1307fe299ed401e9c50ce489d01a13a0f02016
JuTaK97 <didwngus01@snu.ac.kr>
2023-03-19 18:12:55 +0900

    SearchTopBar에 spacedBy로 horizontalArrangement 지정

--- app/src/main/java/com/wafflestudio/snutt2/components/compose/TopBar.kt
+++ app/src/main/java/com/wafflestudio/snutt2/components/compose/TopBar.kt
@@ -117,7 +117,8 @@ fun SearchTopBar(
                 .background(SNUTTColors.Gray100, shape = RoundedCornerShape(6.dp))
                 .fillMaxSize()
                 .padding(horizontal = 8.dp, vertical = 3.dp),
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
         ) {
             content()
         }
