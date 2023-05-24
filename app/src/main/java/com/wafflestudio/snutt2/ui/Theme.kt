fabb65894505e0a20723e7b5493af320421f4adb
JuTaK97 <louis@vcnc.co.kr>
2023-05-25 00:24:34 +0900

    다크모드에서 navigate하면 깜빡이는 이슈 해결

--- app/src/main/java/com/wafflestudio/snutt2/ui/Theme.kt
+++ app/src/main/java/com/wafflestudio/snutt2/ui/Theme.kt
@@ -1,10 +1,13 @@
 package com.wafflestudio.snutt2.ui
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.wafflestudio.snutt2.R
 import com.wafflestudio.snutt2.views.LocalThemeState
 
 private val LightThemeColors @Composable get() = lightColors(
@@ -46,6 +49,15 @@ fun isDarkMode(): Boolean {
 fun SNUTTTheme(
     content: @Composable () -> Unit
 ) {
+    /* <다크모드에서 내비게이션 시 흰색 깜빡이는 이슈 해결>
+     * 내비게이션 시 액티비티 배경색인 흰색(styles.xml에서 android:windowBackground 로 지정된 색)이 잠깐 노출된다.
+     * 원래는 values-night/styles.xml를 통해 다크모드의 색을 지정하지만, 우리는 시스템의 테마와 앱의 테마를
+     * 다르게 설정할 수 있기 때문에 여기서 직접 설정해 준다.
+     */
+    (LocalContext.current as Activity).window.setBackgroundDrawableResource(
+        if (isDarkMode()) R.color.black_dark
+        else R.color.white
+    )
     MaterialTheme(
         colors = if (isDarkMode()) DarkThemeColors else LightThemeColors,
         typography = SNUTTTypography,
