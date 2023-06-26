78dbbade9bff387200c4476894fff961abfd56d0
JuTaK97 <louis@vcnc.co.kr>
2023-06-26 23:52:00 +0900

    RN을 위한 gradle 설정 및 버전 업

---
+++ app/src/main/java/com/wafflestudio/snutt2/views/RNModuleActivity.kt
@@ -0,0 +1,12 @@
+package com.wafflestudio.snutt2.views
+
+import android.app.Activity
+import android.os.Bundle
+
+class RNModuleActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
