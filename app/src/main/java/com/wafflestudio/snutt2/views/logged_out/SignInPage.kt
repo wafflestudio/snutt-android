9c48f7c3cb9ffebb99adb46e1b1c549334e4836c
eastshine2741 <ehdduq8635@naver.com>
2023-07-07 17:27:13 +0900

    SignInPage 수정

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_out/SignInPage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_out/SignInPage.kt
@@ -25,9 +25,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.wafflestudio.snutt2.R
-import com.wafflestudio.snutt2.components.compose.BorderButton
-import com.wafflestudio.snutt2.components.compose.EditText
-import com.wafflestudio.snutt2.components.compose.clicks
+import com.wafflestudio.snutt2.components.compose.*
 import com.wafflestudio.snutt2.lib.facebookLogin
 import com.wafflestudio.snutt2.ui.SNUTTColors
 import com.wafflestudio.snutt2.ui.SNUTTTypography
@@ -66,164 +64,83 @@ fun SignInPage() {
             }
         }
     }
-    val handleFacebookSignIn = {
-        coroutineScope.launch {
-            try {
-                apiOnProgress.showProgress(context.getString(R.string.sign_in_sign_in_button))
-                val loginResult = facebookLogin(context)
-                userViewModel.loginFacebook(
-                    loginResult.accessToken.userId,
-                    loginResult.accessToken.token
-                )
-                homeViewModel.refreshData()
-                navController.navigateAsOrigin(NavigationDestination.Home)
-            } catch (e: Exception) {
-                apiOnError(e)
-            } finally {
-                apiOnProgress.hideProgress()
-            }
-        }
-    }
 
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
             .fillMaxSize()
             .background(SNUTTColors.White900)
-            .padding(30.dp)
             .clicks { focusManager.clearFocus() }
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Image(
-                painter = painterResource(id = R.drawable.logo),
-                contentDescription = stringResource(R.string.sign_in_logo_title),
-                modifier = Modifier.padding(top = 20.dp, bottom = 15.dp),
-            )
-
-            Text(
-                text = stringResource(R.string.sign_in_logo_title),
-                style = SNUTTTypography.h1,
-            )
-        }
+        SimpleTopBar(
+            title = stringResource(R.string.sign_in_app_bar_title),
+            onClickNavigateBack = { navController.popBackStack() }
+        )
 
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(20.dp),
             modifier = Modifier
-                .weight(1f)
-                .padding(top = 50.dp, bottom = 20.dp)
-                .verticalScroll(rememberScrollState())
+                .padding(20.dp)
         ) {
-            EditText(
-                value = idField,
-                onValueChange = { idField = it },
-                hint = stringResource(R.string.sign_in_id_hint),
-                keyboardActions = KeyboardActions(onNext = { focusManager.moveFocus(FocusDirection.Down) }),
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth(),
-            )
-
-            EditText(
-                value = passwordField,
-                onValueChange = { passwordField = it },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                keyboardActions = KeyboardActions(onDone = { handleLocalSignIn() }),
-                hint = stringResource(R.string.sign_in_password_hint),
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth(),
-            )
-        }
-
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier.wrapContentHeight()
-        ) {
-            BorderButton(
-                modifier = Modifier.fillMaxWidth(),
-                color = SNUTTColors.Gray200,
-                onClick = { handleLocalSignIn() }
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                modifier = Modifier
+                    .weight(1f)
             ) {
-                Text(
-                    text = stringResource(R.string.sign_in_sign_in_button),
-                    style = SNUTTTypography.button
-                )
-            }
-
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Box(
-                    modifier = Modifier
-                        .background(SNUTTColors.Gray100)
-                        .height(1.dp)
-                        .weight(1f)
+                EditTextWithTitle(
+                    title = stringResource(R.string.sign_in_id_title),
+                    value = idField,
+                    onValueChange = { idField = it },
+                    hint = stringResource(R.string.sign_in_id_hint),
+                    keyboardActions = KeyboardActions(onNext = { focusManager.moveFocus(FocusDirection.Down) }),
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
                 )
 
-                Text(
-                    modifier = Modifier.padding(horizontal = 8.dp),
-                    text = "or", style = SNUTTTypography.body2, color = SNUTTColors.Gray200
+                EditTextWithTitle(
+                    title = stringResource(R.string.sign_in_password_title),
+                    value = passwordField,
+                    onValueChange = { passwordField = it },
+                    hint = stringResource(R.string.sign_in_password_hint),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    keyboardActions = KeyboardActions(onDone = { handleLocalSignIn() }),
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
                 )
 
-                Box(
-                    modifier = Modifier
-                        .background(SNUTTColors.Gray100)
-                        .height(1.dp)
-                        .weight(1f)
-                )
-            }
-
-            BorderButton(
-                modifier = Modifier.fillMaxWidth(),
-                color = SNUTTColors.FacebookBlue,
-                onClick = { handleFacebookSignIn() }
-            ) {
                 Row(verticalAlignment = Alignment.CenterVertically) {
-                    Image(
-                        painter = painterResource(id = R.drawable.iconfacebook),
-                        contentDescription = stringResource(id = R.string.sign_in_sign_in_facebook_button),
-                        modifier = Modifier
-                            .height(18.dp)
-                            .padding(end = 12.dp),
+                    Text(
+                        text = stringResource(R.string.sign_in_find_id_button),
+                        style = SNUTTTypography.subtitle2,
+                        textDecoration = TextDecoration.Underline,
+                        modifier = Modifier.clicks { navController.navigate(NavigationDestination.FindId) }
                     )
 
                     Text(
-                        text = stringResource(R.string.sign_in_sign_in_facebook_button),
-                        color = SNUTTColors.FacebookBlue,
-                        style = SNUTTTypography.button
+                        text = "|",
+                        style = SNUTTTypography.subtitle2,
+                        modifier = Modifier.padding(horizontal = 15.dp)
+                    )
+
+                    Text(
+                        text = stringResource(R.string.sign_in_find_password_button),
+                        style = SNUTTTypography.subtitle2,
+                        textDecoration = TextDecoration.Underline,
+                        modifier = Modifier.clicks { navController.navigate(NavigationDestination.FindPassword) }
                     )
                 }
             }
 
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Box(
-                    modifier = Modifier
-                        .background(SNUTTColors.Gray100)
-                        .height(1.dp)
-                        .weight(1f)
-                )
-            }
-
-            Row(verticalAlignment = Alignment.CenterVertically) {
+            WebViewStyleButton(
+                color = if (idField.isEmpty() || passwordField.isEmpty()) SNUTTColors.Gray400 else SNUTTColors.SNUTTTheme,
+                onClick = { handleLocalSignIn() },
+                modifier = Modifier
+                    .height(45.dp)
+                    .fillMaxWidth()
+            ) {
                 Text(
-                    text = stringResource(R.string.sign_in_find_id_button),
-                    style = SNUTTTypography.subtitle2,
-                    textDecoration = TextDecoration.Underline,
-                    modifier = Modifier.clicks { navController.navigate(NavigationDestination.FindId) }
-                )
-
-                Text(
-                    text = "|",
-                    style = SNUTTTypography.subtitle2,
-                    modifier = Modifier.padding(horizontal = 15.dp)
-                )
-
-                Text(
-                    text = stringResource(R.string.sign_in_find_password_button),
-                    style = SNUTTTypography.subtitle2,
-                    textDecoration = TextDecoration.Underline,
-                    modifier = Modifier.clicks { navController.navigate(NavigationDestination.FindPassword) }
+                    text = stringResource(R.string.sign_in_sign_in_button),
+                    style = SNUTTTypography.button.copy(color = SNUTTColors.AllWhite)
                 )
             }
         }
