2c8b83f3af1075efe84eced1ba6c8bd837a33ffa
eastshine2741 <ehdduq8635@naver.com>
2023-07-10 01:07:24 +0900

    lint

--- app/src/main/java/com/wafflestudio/snutt2/components/compose/WebViewStyleButton.kt
+++ app/src/main/java/com/wafflestudio/snutt2/components/compose/WebViewStyleButton.kt
@@ -3,12 +3,10 @@ package com.wafflestudio.snutt2.components.compose
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.wafflestudio.snutt2.ui.SNUTTColors
 
--- app/src/main/java/com/wafflestudio/snutt2/views/logged_out/EmailVerificationPage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_out/EmailVerificationPage.kt
@@ -19,8 +19,6 @@ import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.wafflestudio.snutt2.R
