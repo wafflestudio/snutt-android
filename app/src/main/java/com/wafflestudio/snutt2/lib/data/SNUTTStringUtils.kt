d17eb00e94316c3cdb025d64eedbb70d1340f074
eastshine2741 <eastshine@snu.ac.kr>
2023-07-23 00:23:45 +0900

    알림 n분전 텍스트 추가 (#189)

--- app/src/main/java/com/wafflestudio/snutt2/lib/data/SNUTTStringUtils.kt
+++ app/src/main/java/com/wafflestudio/snutt2/lib/data/SNUTTStringUtils.kt
@@ -1,5 +1,7 @@
 package com.wafflestudio.snutt2.lib.data
 
+import android.content.Context
+import com.wafflestudio.snutt2.R
 import com.wafflestudio.snutt2.SNUTTUtils
 import com.wafflestudio.snutt2.lib.network.dto.core.ClassTimeDto
 import com.wafflestudio.snutt2.lib.network.dto.core.LectureDto
@@ -50,7 +52,7 @@ object SNUTTStringUtils {
         return text.toString()
     }
 
-    fun getNotificationTime(info: NotificationDto): String {
+    fun getNotificationTime(context: Context, info: NotificationDto): String {
         try {
             val format: DateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
             format.timeZone = TimeZone.getTimeZone("UTC")
@@ -59,16 +61,20 @@ object SNUTTStringUtils {
 
             val diff = date2.time - date1.time
             val hours = diff / (1000 * 60 * 60)
+            val minutes = diff / (1000 * 60)
             val days = hours / 24
             return when {
                 days > 0 -> {
                     DateFormat.getDateInstance().format(date1)
                 }
                 hours > 0 -> {
-                    "$hours 시간 전" // TODO: resource로 빼기
+                    context.getString(R.string.time_hours_ago, hours)
+                }
+                minutes > 0 -> {
+                    context.getString(R.string.time_minutes_ago, minutes)
                 }
                 else -> {
-                    "방금"
+                    context.getString(R.string.time_now)
                 }
             }
         } catch (e: ParseException) {
