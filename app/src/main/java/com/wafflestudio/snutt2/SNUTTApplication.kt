63aba41d2b731b81bc10806a55b33d6e24f6d489
JuTaK97 <louis@vcnc.co.kr>
2023-08-12 03:20:22 +0900

    불필요한 변화 3

--- app/src/main/java/com/wafflestudio/snutt2/SNUTTApplication.kt
+++ app/src/main/java/com/wafflestudio/snutt2/SNUTTApplication.kt
@@ -3,10 +3,6 @@ package com.wafflestudio.snutt2
 import android.app.Application
 import android.content.res.Configuration
 import androidx.compose.animation.ExperimentalAnimationApi
-import com.facebook.react.ReactApplication
-import com.facebook.react.ReactNativeHost
-import com.facebook.react.ReactPackage
-import com.facebook.react.shell.MainReactPackage
 import com.wafflestudio.snutt2.lib.rx.DirectFirstHandleScheduler
 import com.wafflestudio.snutt2.provider.TimetableWidgetProvider
 import dagger.hilt.android.HiltAndroidApp
@@ -18,7 +14,7 @@ import timber.log.Timber
  * Created by makesource on 2016. 1. 17..
  */
 @HiltAndroidApp
-class SNUTTApplication : Application(), ReactApplication {
+class SNUTTApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
@@ -33,16 +29,6 @@ class SNUTTApplication : Application(), ReactApplication {
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
         TimetableWidgetProvider.refreshWidget(applicationContext)
-    }
-
-    override fun getReactNativeHost(): ReactNativeHost {
-        return object : ReactNativeHost(this) {
-            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-            override fun getPackages(): List<ReactPackage> = listOf<ReactPackage>(MainReactPackage())
-
-            override fun getJSMainModuleName(): String = "friends"
-        }
     }
 
     companion object {
