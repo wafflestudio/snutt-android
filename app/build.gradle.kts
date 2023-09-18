8ac9023e45c3e80ea8f083f6ab13fa0e17204e2c
양주현 <didwngus01@naver.com>
2023-09-19 03:00:45 +0900

    release snutt 3.3.0 (#223)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,8 @@ android {
         }
 
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
