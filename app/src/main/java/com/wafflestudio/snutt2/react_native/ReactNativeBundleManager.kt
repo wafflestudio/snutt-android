14ba8101c37448fddbec17e9bb468db85694cdf5
양주현 <didwngus01@naver.com>
2023-12-10 00:44:27 +0900

    async-storage 패키지 추가

--- app/src/main/java/com/wafflestudio/snutt2/react_native/ReactNativeBundleManager.kt
+++ app/src/main/java/com/wafflestudio/snutt2/react_native/ReactNativeBundleManager.kt
@@ -11,6 +11,7 @@ import com.facebook.react.ReactRootView
 import com.facebook.react.common.LifecycleState
 import com.facebook.react.shell.MainReactPackage
 import com.horcrux.svg.SvgPackage
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage
 import com.reactnativecommunity.picker.RNCPickerPackage
 import com.swmansion.gesturehandler.RNGestureHandlerPackage
 import com.swmansion.reanimated.ReanimatedPackage
@@ -69,7 +70,7 @@ class ReactNativeBundleManager @Inject constructor(
                                 .setJavaScriptExecutorFactory(HermesExecutorFactory())
                                 .setJSBundleFile(bundleFile.absolutePath)
                                 .addPackages(
-                                    listOf(MainReactPackage(), RNGestureHandlerPackage(), ReanimatedPackage(), SafeAreaContextPackage(), RNCPickerPackage(), SvgPackage()),
+                                    listOf(MainReactPackage(), RNGestureHandlerPackage(), ReanimatedPackage(), SafeAreaContextPackage(), RNCPickerPackage(), SvgPackage(), AsyncStoragePackage()),
                                 )
                                 .setInitialLifecycleState(LifecycleState.RESUMED)
                                 .build()
