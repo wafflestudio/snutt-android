d7b4e5d8462301ac848ca3ab20d39a96fa70fd7e
eastshine2741 <eastshine@snu.ac.kr>
2024-01-17 02:03:45 +0900

    Staging releaseNotes 추가(나중에 지우기)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,7 @@ android {
                 artifactType = "APK"
                 testers = "urban"
                 serviceCredentialsFile = "gcp-service-account-staging.json"
+                releaseNotes = "Custom theme release test"
             }
         }
 
