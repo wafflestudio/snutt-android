a915ddaafe791a97069a9e184eb9e8d1650f31cc
eastshine2741 <eastshine@snu.ac.kr>
2024-01-20 20:16:21 +0900

    ColorBox 디폴트 파라미터 추가

--- app/src/main/java/com/wafflestudio/snutt2/components/compose/ColorBox.kt
+++ app/src/main/java/com/wafflestudio/snutt2/components/compose/ColorBox.kt
@@ -4,12 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.wafflestudio.snutt2.lib.network.dto.core.ColorDto
 import com.wafflestudio.snutt2.ui.SNUTTColors
@@ -17,26 +17,30 @@ import com.wafflestudio.snutt2.ui.SNUTTColors
 @Composable
 fun ColorBox(
     color: ColorDto,
+    modifier: Modifier = Modifier,
+    borderColor: Color = SNUTTColors.Black250,
+    borderThickness: Dp = 0.5.dp,
 ) {
     Row(
-        modifier = Modifier
-            .width(40.dp)
-            .height(20.dp)
-            .border(width = (0.5f).dp, color = SNUTTColors.Black250),
+        modifier = modifier
+            .border(width = borderThickness, color = borderColor)
+            .size(width = 40.dp, height = 20.dp),
     ) {
         Box(
             modifier = Modifier
                 .background(
                     Color(color.fgColor ?: 0xffffff),
                 )
-                .size(20.dp),
+                .weight(1f)
+                .fillMaxHeight(),
         )
         Box(
             modifier = Modifier
                 .background(
                     Color(color.bgColor ?: 0xffffff),
                 )
-                .size(20.dp),
+                .weight(1f)
+                .fillMaxHeight(),
         )
     }
 }
