c09c17ee71d3eb62d246fcd95d6d579892819d04
양주현 <didwngus01@naver.com>
2024-01-24 01:48:08 +0900

    lint

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/search/search_option/TagsColumn.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/search/search_option/TagsColumn.kt
@@ -13,9 +13,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
