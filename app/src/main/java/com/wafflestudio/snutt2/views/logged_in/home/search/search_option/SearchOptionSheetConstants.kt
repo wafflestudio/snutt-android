aa4ea0503f73ee8c197518cafcb80dd521ed9274
양주현 <didwngus01@naver.com>
2024-01-25 23:27:32 +0900

    다크모드 색상 별도 지정

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/search/search_option/SearchOptionSheetConstants.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/search/search_option/SearchOptionSheetConstants.kt
@@ -3,8 +3,10 @@ package com.wafflestudio.snutt2.views.logged_in.home.search.search_option
 import android.graphics.Color
 import androidx.annotation.ColorInt
 import androidx.compose.animation.core.spring
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import com.wafflestudio.snutt2.lib.network.dto.core.ColorDto
+import com.wafflestudio.snutt2.ui.isDarkMode
 
 object SearchOptionSheetConstants {
     const val TagColumnWidthDp = 120
@@ -16,8 +18,16 @@ object SearchOptionSheetConstants {
     )
 
     @ColorInt val BackgroundLectureBlockColor = Color.argb(153, 27, 208, 200)
-    val TimeBlockColor = ColorDto(
-        fgRaw = "#FFFFFF",
-        bgRaw = "#B3DADADA",
-    )
+    val TimeBlockColor: ColorDto
+        @Composable get() = if (isDarkMode()) {
+            ColorDto(
+                fgRaw = "#BBBBBB",
+                bgRaw = "#B3666666",
+            )
+        } else {
+            ColorDto(
+                fgRaw = "#FFFFFF",
+                bgRaw = "#B3DADADA",
+            )
+        }
 }
