6b26327ef6ec92a695cd9aa1f4e5f26a7a733773
양주현 <didwngus01@naver.com>
2024-04-03 22:19:15 +0900

    플로팅 버튼 디자인 수정

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/lecture_detail/deeplink/DeeplinkLectureDetailPage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/lecture_detail/deeplink/DeeplinkLectureDetailPage.kt
@@ -1,7 +1,11 @@
 package com.wafflestudio.snutt2.views.logged_in.lecture_detail.deeplink
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -11,7 +15,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.wafflestudio.snutt2.R
+import com.wafflestudio.snutt2.components.compose.ArrowLeftBold
+import com.wafflestudio.snutt2.components.compose.LogoIcon
 import com.wafflestudio.snutt2.components.compose.clicks
 import com.wafflestudio.snutt2.ui.SNUTTColors
 import com.wafflestudio.snutt2.ui.SNUTTTypography
@@ -39,26 +50,52 @@ fun DeeplinkLectureDetailPage(
             },
         )
         if (tableId != null) {
-            Box(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(bottom = 15.dp)
-                    .background(shape = RoundedCornerShape(30.dp), color = SNUTTColors.Gray20)
-                    .size(width = 150.dp, height = 40.dp)
-                    .clicks {
-                        scope.launch {
-                            tableListViewModel.changeSelectedTable(tableId)
-                            navController.navigate(NavigationDestination.Home) {
-                                popUpTo(NavigationDestination.Home) {
-                                    inclusive = false
-                                }
+            FloatingButton(
+                onClick = {
+                    scope.launch {
+                        tableListViewModel.changeSelectedTable(tableId)
+                        navController.navigate(NavigationDestination.Home) {
+                            popUpTo(NavigationDestination.Home) {
+                                inclusive = false
                             }
                         }
-                    },
-                contentAlignment = Alignment.Center,
-            ) {
-                Text(text = "해당 시간표로 이동", style = SNUTTTypography.body1)
-            }
+                    }
+                },
+            )
         }
+    }
+}
+
+@Composable
+private fun BoxScope.FloatingButton(
+    onClick: () -> Unit,
+) {
+    val shape = RoundedCornerShape(25.dp)
+
+    Row(
+        modifier = Modifier
+            .align(Alignment.BottomCenter)
+            .padding(bottom = 16.dp)
+            .shadow(3.dp, shape)
+            .clip(shape)
+            .background(SNUTTColors.White900)
+            .border(1.dp, SNUTTColors.SNUTTTheme, shape)
+            .padding(start = 8.dp, top = 8.dp, bottom = 8.dp, end = 16.dp)
+            .clicks {
+                onClick()
+            },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(2.dp),
+    ) {
+        ArrowLeftBold(
+            modifier = Modifier.size(23.dp),
+            colorFilter = ColorFilter.tint(SNUTTColors.Black900),
+        )
+        LogoIcon(modifier = Modifier.size(13.dp))
+        Text(
+            text = stringResource(R.string.deeplink_page_timetable_lecture_page_floating_button),
+            style = SNUTTTypography.h4,
+            color = SNUTTColors.Black900,
+        )
     }
 }
