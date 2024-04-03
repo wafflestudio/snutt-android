b576c871718a7969d8eb2c855ce360b20ce4f75c
양주현 <didwngus01@naver.com>
2024-04-03 23:01:59 +0900

    Lint

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/lecture_detail/deeplink/DeeplinkLectureDetailPage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/lecture_detail/deeplink/DeeplinkLectureDetailPage.kt
@@ -1,7 +1,6 @@
 package com.wafflestudio.snutt2.views.logged_in.lecture_detail.deeplink
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
@@ -15,14 +14,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.wafflestudio.snutt2.R
 import com.wafflestudio.snutt2.components.compose.ArrowLeftBold
-import com.wafflestudio.snutt2.components.compose.LogoIcon
 import com.wafflestudio.snutt2.components.compose.clicks
 import com.wafflestudio.snutt2.ui.SNUTTColors
 import com.wafflestudio.snutt2.ui.SNUTTTypography
