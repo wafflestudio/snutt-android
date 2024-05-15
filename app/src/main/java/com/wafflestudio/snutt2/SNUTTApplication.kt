8c4666be0f00a98345982bc955b5a39849615bac
양주현 <didwngus01@naver.com>
2024-05-15 17:59:48 +0900

    카카오 SDK 설정

--- app/src/main/java/com/wafflestudio/snutt2/SNUTTApplication.kt
+++ app/src/main/java/com/wafflestudio/snutt2/SNUTTApplication.kt
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.JavaScriptExecutorFactory
 import com.facebook.react.shell.MainReactPackage
 import com.horcrux.svg.SvgPackage
+import com.kakao.sdk.common.KakaoSdk
 import com.naver.maps.map.NaverMapSdk
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage
 import com.reactnativecommunity.picker.RNCPickerPackage
@@ -32,6 +33,7 @@ class SNUTTApplication : Application(), ReactApplication {
         Timber.plant(Timber.DebugTree())
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NaverCloudPlatformClient(getString(R.string.naver_map_client_id))
+        KakaoSdk.init(this, getString(R.string.kakao_native_app_key))
     }
 
     @OptIn(ExperimentalAnimationApi::class)
--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ allprojects {
         maven(url = "https://jitpack.io")
         maven("https://naver.jfrog.io/artifactory/maven/")
         maven("https://repository.map.naver.com/archive/maven")
+        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
     }
 }
 
