878abd338eb12e4811362e700187dba44aadb3fe
eastshine2741 <eastshine@snu.ac.kr>
2024-05-18 15:17:49 +0900

    DTO에서 parcelable, parcelize 제거

--- app/src/main/java/com/wafflestudio/snutt2/lib/network/dto/core/ColorDto.kt
+++ app/src/main/java/com/wafflestudio/snutt2/lib/network/dto/core/ColorDto.kt
@@ -1,17 +1,14 @@
 package com.wafflestudio.snutt2.lib.network.dto.core
 
 import android.graphics.Color
-import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import kotlinx.parcelize.Parcelize
 
 @JsonClass(generateAdapter = true)
-@Parcelize
 data class ColorDto(
     @Json(name = "fg") val fgRaw: String? = null,
     @Json(name = "bg") val bgRaw: String? = null,
-) : Parcelable {
+) {
     constructor(fgColor: Int, bgColor: Int) : this(
         "#%06X".format(0xFFFFFF and fgColor),
         "#%06X".format(0xFFFFFF and bgColor),
--- app/src/main/java/com/wafflestudio/snutt2/model/GeoCoordinate.kt
+++ app/src/main/java/com/wafflestudio/snutt2/model/GeoCoordinate.kt
@@ -1,16 +1,13 @@
 package com.wafflestudio.snutt2.model
 
-import android.os.Parcelable
 import com.naver.maps.geometry.LatLng
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import kotlinx.parcelize.Parcelize
 
 @JsonClass(generateAdapter = true)
-@Parcelize
 data class GeoCoordinate(
     @Json(name = "latitude") val latitude: Double,
     @Json(name = "longitude") val longitude: Double,
-) : Parcelable {
+) {
     fun toLatLng(): LatLng = LatLng(latitude, longitude)
 }
