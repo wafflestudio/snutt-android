876c91f34c7e1549e91a72c209526ea4635aa695
양주현 <didwngus01@naver.com>
2024-06-21 00:19:08 +0900

    리팩토링 브랜치에서 targetSdk 34일때 RN 런타임 크래시 발생하는 것 임시 해결 (#287)

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -14,7 +14,7 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
 
             extensions.configure<ApplicationExtension> {
                 configureKotlinAndroid(this)
-                defaultConfig.targetSdk = 34
+                defaultConfig.targetSdk = 33
             }
         }
     }
