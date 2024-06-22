3e49458fdfef95139fb4a51bf8908370eccb249f
plgafhd <plgafhd@snu.ac.kr>
2024-06-23 01:47:36 +0900

    필요 없는 코드 지우기

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,41 +1,10 @@
 plugins {
-    id("org.jetbrains.kotlin.android")
     alias(libs.plugins.android.library)
     alias(libs.plugins.hilt)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
 android {
     namespace = "com.wafflestudio.snutt2.core.network"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
