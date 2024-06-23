7a1b314e3aa3335a14f9b326279f2e84e9e63c7f
eastshine2741 <eastshine@snu.ac.kr>
2024-06-23 14:22:00 +0900

    User에 id 추가

--- core/database/src/main/java/com/wafflestudio/snutt2/core/database/model/User.kt
+++ core/database/src/main/java/com/wafflestudio/snutt2/core/database/model/User.kt
@@ -2,8 +2,8 @@ package com.wafflestudio.snutt2.core.database.model
 
 import com.squareup.moshi.Json
 
-// TODO: 스펙 변경 반영
 data class User(
+    @Json(name = "id") val id: String? = null,
     @Json(name = "isAdmin") val isAdmin: Boolean = false,
     @Json(name = "regDate") val regDate: String? = null,
     @Json(name = "notificationCheckedAt") val notificationCheckedAt: String? = null,
