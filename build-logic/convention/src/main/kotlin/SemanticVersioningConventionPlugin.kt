460d15aaaef7bfe010dda94bc7d03338bee79e30
양주현 <didwngus01@naver.com>
2024-06-23 21:14:43 +0900

    [멀티 모듈 리팩토링] versionCode, versionName 관련 설정 및 serializedCode 생성 로직을 별도 플러그인으로 분리 (#286)

---
+++ build-logic/convention/src/main/kotlin/SemanticVersioningConventionPlugin.kt
@@ -0,0 +1,40 @@
+import com.android.build.gradle.AppExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.Properties
+
+class SemanticVersioningConventionPlugin: Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            extensions.configure<AppExtension> {
+                val versionProps = Properties().apply {
+                    load(Files.newBufferedReader(Paths.get(rootProject.rootDir.toString(), "version.properties")))
+                }
+
+                defaultConfig {
+                    val propertyVersionName = versionProps.getProperty("snuttVersionName")
+                    versionName = propertyVersionName
+                    versionCode = semanticVersionToSerializedCode(propertyVersionName)
+                }
+            }
+        }
+    }
+
+    private fun semanticVersionToSerializedCode(semanticVersion: String): Int {
+
+        val semVerRegex = Regex("(\\d+).(\\d+).(\\d+)(-rc.(\\d+))?")
+
+        val major = semVerRegex.find(semanticVersion)?.groupValues?.get(1)?.toIntOrNull() ?: 0
+        val minor = semVerRegex.find(semanticVersion)?.groupValues?.get(2)?.toIntOrNull() ?: 0
+        val patch = semVerRegex.find(semanticVersion)?.groupValues?.get(3)?.toIntOrNull() ?: 0
+
+        return listOf(major, minor, patch)
+            .fold(0) { acc, next ->
+                acc * 100 + next
+            } + 2010000000
+    }
+}
