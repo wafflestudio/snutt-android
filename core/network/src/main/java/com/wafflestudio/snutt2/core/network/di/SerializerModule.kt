bfd5f5dba50c8bcbcf859f415903a3b7ac3a9d7f
plgafhd <plgafhd@snu.ac.kr>
2024-06-29 00:01:08 +0900

    provideMoshi SerializerModule로 이동

--- core/network/src/main/java/com/wafflestudio/snutt2/core/network/di/SerializerModule.kt
+++ core/network/src/main/java/com/wafflestudio/snutt2/core/network/di/SerializerModule.kt
@@ -1,11 +1,15 @@
 package com.wafflestudio.snutt2.core.network.di
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.wafflestudio.snutt2.core.network.util.MoshiSerializer
 import com.wafflestudio.snutt2.core.network.util.Serializer
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -13,4 +17,12 @@ abstract class SerializerModule {
 
     @Binds
     abstract fun bindSerializer(moshiSerializer: MoshiSerializer): Serializer
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
 }
