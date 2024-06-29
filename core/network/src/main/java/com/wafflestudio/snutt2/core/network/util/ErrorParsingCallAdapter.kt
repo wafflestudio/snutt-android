a9a4e8296ea4e7ae21c8add203e5f907bdc65763
eastshine2741 <eastshine@snu.ac.kr>
2024-06-29 19:43:19 +0900

    import문 수정

--- core/network/src/main/java/com/wafflestudio/snutt2/core/network/util/ErrorParsingCallAdapter.kt
+++ core/network/src/main/java/com/wafflestudio/snutt2/core/network/util/ErrorParsingCallAdapter.kt
@@ -1,6 +1,6 @@
 package com.wafflestudio.snutt2.core.network.util
 
-import com.wafflestudio.snutt2.core.network.ErrorDTO
+import com.wafflestudio.snutt2.core.network.model.ErrorDTO
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Maybe
 import io.reactivex.rxjava3.core.Single
