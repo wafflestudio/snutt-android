96e341979274029b14f20958f0395fe3bd3314dc
plgafhd <plgafhd@snu.ac.kr>
2024-07-02 15:49:30 +0900

    bind 함수 작성

--- core/network/src/main/java/com/wafflestudio/snutt2/core/network/di/SerializerModule.kt
+++ core/network/src/main/java/com/wafflestudio/snutt2/core/network/di/SerializerModule.kt
@@ -2,6 +2,8 @@ package com.wafflestudio.snutt2.core.network.di
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.wafflestudio.snutt2.core.network.SNUTTNetworkDataSource
+import com.wafflestudio.snutt2.core.network.retrofit.RetrofitSNUTTNetwork
 import com.wafflestudio.snutt2.core.network.util.MoshiSerializer
 import com.wafflestudio.snutt2.core.network.util.Serializer
 import com.wafflestudio.snutt2.core.qualifiers.CoreNetwork
@@ -19,6 +21,10 @@ abstract class SerializerModule {
     @Binds
     @CoreNetwork
     abstract fun bindSerializer(moshiSerializer: MoshiSerializer): Serializer
+
+    @Binds
+    @CoreNetwork
+    abstract fun bindNetwork(impl: RetrofitSNUTTNetwork): SNUTTNetworkDataSource
 
     companion object {
         @Provides
