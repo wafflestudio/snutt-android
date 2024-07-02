91602a8c0e37894536c06f5e17ae4657a50743db
plgafhd <plgafhd@snu.ac.kr>
2024-07-03 02:18:29 +0900

    VacancyRepository 변경

--- app/src/main/java/com/wafflestudio/snutt2/lib/network/dto/GetVacancyLecturesResults.kt
+++ app/src/main/java/com/wafflestudio/snutt2/lib/network/dto/GetVacancyLecturesResults.kt
@@ -2,9 +2,15 @@ package com.wafflestudio.snutt2.lib.network.dto
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import com.wafflestudio.snutt2.core.network.model.GetVacancyLecturesResults as GetVacancyLecturesResultsNetwork
 import com.wafflestudio.snutt2.lib.network.dto.core.LectureDto
+import com.wafflestudio.snutt2.lib.network.dto.core.toExternalModel
 
 @JsonClass(generateAdapter = true)
 data class GetVacancyLecturesResults(
     @Json(name = "lectures") val lectures: List<LectureDto>,
+)
+
+fun GetVacancyLecturesResultsNetwork.toExternalModel() = GetVacancyLecturesResults(
+    lectures = this.lectures.map { it.toExternalModel() },
 )
