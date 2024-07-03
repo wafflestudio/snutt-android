980619897a74bb9dbeef8df7e7a9b61c421c5d68
plgafhd <plgafhd@snu.ac.kr>
2024-07-03 16:58:32 +0900

    DebugViewModel에 적용 (네트워크 로그가 나온다)

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/DebugViewModel.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/DebugViewModel.kt
@@ -1,19 +1,29 @@
 package com.wafflestudio.snutt2.views.logged_in.home.settings
 
 import androidx.lifecycle.ViewModel
-import com.wafflestudio.snutt2.core.qualifiers.App
+import com.wafflestudio.snutt2.core.database.model.NetworkLog as NetworkLogDatabase
+import com.wafflestudio.snutt2.core.database.preference.SNUTTStorageTemp
+import com.wafflestudio.snutt2.core.database.util.map
+import com.wafflestudio.snutt2.core.qualifiers.CoreDatabase
 import com.wafflestudio.snutt2.data.SNUTTStorage
 import com.wafflestudio.snutt2.lib.network.NetworkLog
+import com.wafflestudio.snutt2.lib.network.toExternalModel
+import com.wafflestudio.snutt2.model.toExternalModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class DebugViewModel @Inject constructor(
-    @App private val snuttStorage: SNUTTStorage,
+    @CoreDatabase private val snuttStorage: SNUTTStorageTemp,
+    externalScope: CoroutineScope, // TODO : 임시로 필요
 ) : ViewModel() {
 
     val networkLog: StateFlow<List<NetworkLog>> = snuttStorage.networkLog.asStateFlow()
+        .map(externalScope) {it: List<NetworkLogDatabase> -> it.map { log ->
+            log.toExternalModel()}
+        }
 
     fun clearNetworkLog() {
         snuttStorage.networkLog.clear()
