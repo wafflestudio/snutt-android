b4a5e4d16ab63475ac93b53d3626236f70e95b77
eastshine2741 <eastshine@snu.ac.kr>
2024-07-07 19:39:55 +0900

    User, Nickname 추가

---
+++ core/model/src/main/java/com/wafflestudio/snutt2/core/model/data/User.kt
@@ -0,0 +1,9 @@
+package com.wafflestudio.snutt2.core.model.data
+
+data class User(
+    val isAdmin: Boolean,
+    val email: String?,
+    val localId: String?,
+    val facebookName: String?,
+    val nickname: Nickname
+)
