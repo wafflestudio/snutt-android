62fdb99470297ed86032dce33e243b7cc5dfe2f4
eastshine2741 <eastshine@snu.ac.kr>
2024-07-07 19:12:17 +0900

    Lecture 필드 rename

--- core/model/src/main/java/com/wafflestudio/snutt2/core/model/data/lecture/Lecture.kt
+++ core/model/src/main/java/com/wafflestudio/snutt2/core/model/data/lecture/Lecture.kt
@@ -4,17 +4,17 @@ import com.wafflestudio.snutt2.core.model.data.PlaceTime
 
 abstract class Lecture(
     open val id: String,
-    open val classification: String?,
+    open val title: String,
+    open val instructor: String,
     open val department: String?,
     open val academicYear: String?,
-    open val courseNumber: String?,
-    open val lectureNumber: String?,
-    open val courseTitle: String,
     open val credit: Long,
-    open val placeTimes: List<PlaceTime>,
-    open val instructor: String,
+    open val classification: String?,
+    open val category: String?,
+    open val courseNumber: String?,     // 강좌번호
+    open val lectureNumber: String?,    // 분반번호
     open val quota: Long?,
     open val freshmanQuota: Long?,
     open val remark: String,
-    open val category: String?,
+    open val placeTimes: List<PlaceTime>,
 )
