13632a6832431d2680a6ce9adff18b2977bda132
plgafhd <plgafhd@snu.ac.kr>
2024-07-13 18:39:46 +0900

    core:data package 구축

---
+++ core/data/src/main/java/com/wafflestudio/snutt2/core/data/di/DataModule.kt
@@ -0,0 +1,9 @@
+package com.wafflestudio.snutt2.core.data.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataModule
