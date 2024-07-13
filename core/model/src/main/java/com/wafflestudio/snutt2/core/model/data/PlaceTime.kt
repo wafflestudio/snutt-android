0bc8b06f879c1df3d9f2f5d3de83a022acad56cf
plgafhd <plgafhd@snu.ac.kr>
2024-07-13 16:54:17 +0900

    PlaceTime 확장 함수 추가

--- core/model/src/main/java/com/wafflestudio/snutt2/core/model/data/PlaceTime.kt
+++ core/model/src/main/java/com/wafflestudio/snutt2/core/model/data/PlaceTime.kt
@@ -1,5 +1,8 @@
 package com.wafflestudio.snutt2.core.model.data
 
+import kotlin.math.max
+import kotlin.math.min
+
 data class PlaceTime(
     val timetableBlock: TimetableBlock,
     val placeName: String,
@@ -9,16 +12,27 @@ data class TimetableBlock (
     val day: Day,
     val startTime: Time,
     val endTime: Time,
-)
+){
+    fun trimByTrimParam(tableTrimParam: TimeTableTrimConfig): TimetableBlock? {
+        if (tableTrimParam.startDay > day || day > tableTrimParam.endDay) return null
+        if (tableTrimParam.startTime >= endTime || tableTrimParam.endTime <= endTime) return null
 
-enum class Day(fromInt: Int) {
+        return this.copy(
+            day = day,
+            startTime = if (startTime > tableTrimParam.startTime) startTime else tableTrimParam.startTime,
+            endTime = if (endTime < tableTrimParam.endTime) endTime else tableTrimParam.endTime,
+        )
+    }
+}
+
+enum class Day(order: Int) {
     MONDAY(0),
     TUESDAY(1),
     WEDNESDAY(2),
     THURSDAY(3),
     FRIDAY(4),
     SATURDAY(5),
-    SUNDAY(6),
+    SUNDAY(6);
 }
 
 data class Time(
