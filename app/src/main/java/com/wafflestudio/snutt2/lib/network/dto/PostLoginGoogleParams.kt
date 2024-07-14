ccd876dc807780d7ba3a1b9be6370f79a69c2730
plgafhd <plgafhd@snu.ac.kr>
2024-07-14 13:43:16 +0900

    소셜 로그인 api 변경

--- app/src/main/java/com/wafflestudio/snutt2/lib/network/dto/PostLoginGoogleParams.kt
+++
@@ -1,9 +0,0 @@
-package com.wafflestudio.snutt2.lib.network.dto
-
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class PostLoginGoogleParams(
-    @Json(name = "token") val googleIdToken: String,
-)
