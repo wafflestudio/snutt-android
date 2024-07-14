f69fb3ed88e016b6d49b6680e25c2e65044b9f7c
plgafhd <plgafhd@snu.ac.kr>
2024-07-15 01:34:36 +0900

    AuthCodeHandlerActivity 임시 추가

---
+++ app/src/main/java/com/wafflestudio/snutt2/views/AuthCodeHandlerActivity.kt
@@ -0,0 +1,50 @@
+package com.wafflestudio.snutt2.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+
+class AuthCodeHandlerActivity : AppCompatActivity() {
+    private val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (error != null) {
+            // Log.d("aaaa", "로그인 실패 $error") TODO:
+        } else if (token != null) {
+            // Log.d("aaaa", "로그인 성공 ${token.accessToken}") TODO:
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+            // 카카오톡 로그인
+            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                // 로그인 실패 부분
+                if (error != null) {
+                    // Log.d("aaaa", "로그인 실패 $error") TODO:
+                    // 사용자가 취소
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+                    // 다른 오류
+                    else {
+                        UserApiClient.instance.loginWithKakaoAccount(
+                            this,
+                            callback = callback
+                        ) // 카카오 이메일 로그인
+                    }
+                }
+                // 로그인 성공 부분
+                else if (token != null) {
+                    //Log.d("aaaa", "로그인 성공 ${token.accessToken}") // TODO:
+                }
+            }
+        }
+        else {
+            UserApiClient.instance.loginWithKakaoAccount(this, callback = callback) // 카카오 이메일 로그인
+        }
+    }
+}
