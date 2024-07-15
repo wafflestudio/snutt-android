16f09bb6678a3db74f0cf85e000c37b0b2ca5420
plgafhd <plgafhd@snu.ac.kr>
2024-07-15 10:45:47 +0900

    로그인 직전에 기존 연결은 끊기

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_out/TutorialPage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_out/TutorialPage.kt
@@ -162,30 +162,39 @@ fun TutorialPage() {
     }
 
     val handleKaKaoSignin: () -> Unit = {
-        if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
-            UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
-                if (error != null) {
-                    context.toast("실패2!")
-                    Log.d("plgafhd2",error.message.toString())
-                    //Log.e(TAG, "카카오톡으로 로그인 실패", error) // TODO:
+        // 먼저 로그아웃을 시도하여 기존의 로그인 정보를 제거합니다.
+        UserApiClient.instance.unlink { error ->
+            if (error != null) {
+                Log.d("plgafhdLogout", error.message.toString())
+                context.toast("로그아웃 실패!")
+            } else {
+                Log.d("plgafhdLogout", "로그아웃 성공!")
+                if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+                    UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+                        if (error != null) {
+                            context.toast("실패2!")
+                            Log.d("plgafhd2", error.message.toString())
+                            //Log.e(TAG, "카카오톡으로 로그인 실패", error) // TODO:
 
-                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                        return@loginWithKakaoTalk
+                            // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                            // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                            if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                                return@loginWithKakaoTalk
+                            }
+
+                            // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                            UserApiClient.instance.loginWithKakaoAccount(context, callback = kakaoCallback)
+                        } else if (token != null) {
+                            context.toast("성공2!")
+                            Log.d("plgafhd2", token.accessToken.toString())
+                            loginWithKaKaoAccessToken(token.accessToken)
+                            //GoMain() TODO:
+                        }
                     }
-
-                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                } else {
                     UserApiClient.instance.loginWithKakaoAccount(context, callback = kakaoCallback)
-                } else if (token != null) {
-                    context.toast("성공2!")
-                    Log.d("plgafhd2",token.accessToken.toString())
-                    loginWithKaKaoAccessToken(token.accessToken)
-                    //GoMain() TODO:
                 }
             }
-        } else {
-            UserApiClient.instance.loginWithKakaoAccount(context, callback = kakaoCallback)
         }
     }
 
