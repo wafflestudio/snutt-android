9d5482852c5370525c083d2f1f1e560d307320f0
plgafhd <plgafhd@snu.ac.kr>
2024-07-16 17:58:42 +0900

    CourseBook 확장 함수 추가

--- core/model/src/main/java/com/wafflestudio/snutt2/core/model/data/CourseBook.kt
+++ core/model/src/main/java/com/wafflestudio/snutt2/core/model/data/CourseBook.kt
@@ -3,4 +3,17 @@ package com.wafflestudio.snutt2.core.model.data
 data class CourseBook(
     val semester: Long,
     val year: Long,
-)
+): Comparable<CourseBook> {
+    override fun compareTo(other: CourseBook): Int {
+        if (year > other.year) {
+            return -1
+        } else if (year < other.year) {
+            return 1
+        } else {
+            if (semester > other.semester) {
+                return -1
+            } else if (semester < other.semester) return 1
+        }
+        return 0
+    }
+}
