f2b5e679e1a70ed3db32da2289e3cde4284346b4
양주현 <didwngus01@naver.com>
2024-07-20 21:10:06 +0900

    core:model Lecture에 equals, hashCode 오버라이드 추가

--- core/model/src/main/java/com/wafflestudio/snutt2/core/model/data/lecture/Lecture.kt
+++ core/model/src/main/java/com/wafflestudio/snutt2/core/model/data/lecture/Lecture.kt
@@ -40,4 +40,60 @@ abstract class Lecture(
         if (lectureNumber == null) return false
         return isCourseNumberEquals(lecture) && lectureNumber == lecture.lectureNumber
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        } else if (other !is Lecture) {
+            return false
+        } else {
+            return if (id != other.id) {
+                false
+            } else if (originalLectureId != other.originalLectureId) {
+                false
+            } else if (title != other.title) {
+                false
+            } else if (instructor != other.instructor) {
+                false
+            } else if (department != other.department) {
+                false
+            } else if (academicYear != other.academicYear) {
+                false
+            } else if (credit != other.credit) {
+                false
+            } else if (classification != other.classification) {
+                false
+            } else if (category != other.category) {
+                false
+            } else if (courseNumber != other.courseNumber) {
+                false
+            } else if (lectureNumber != other.lectureNumber) {
+                false
+            } else if (quota != other.quota) {
+                false
+            } else if (freshmanQuota != other.freshmanQuota) {
+                false
+            } else if (remark != other.remark) {
+                false
+            } else if (placeTimes.toSet() != other.placeTimes.toSet()) {
+                false
+            } else if (color != other.color) {
+                false
+            } else if (registrationCount != other.registrationCount) {
+                false
+            } else {
+                wasFull != other.wasFull
+            }
+        }
+    }
+
+    override fun hashCode(): Int {
+        return listOf(
+            id, originalLectureId, title, instructor, department,
+            academicYear, credit, classification, category, courseNumber, lectureNumber,
+            quota, freshmanQuota, remark, placeTimes, color, registrationCount, wasFull,
+        ).fold(0) { current, value ->
+            current * 31 + (value?.hashCode() ?: 0)
+        }
+    }
 }
