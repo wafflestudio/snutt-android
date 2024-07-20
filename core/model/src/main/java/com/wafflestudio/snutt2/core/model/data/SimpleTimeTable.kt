fad66d7d388e2fa086d8d52e745f53e3a0e745b4
양주현 <didwngus01@naver.com>
2024-07-20 17:56:41 +0900

    core:model SimpleTimeTable

--- core/model/src/main/java/com/wafflestudio/snutt2/core/model/data/SimpleTimeTable.kt
+++ core/model/src/main/java/com/wafflestudio/snutt2/core/model/data/SimpleTimeTable.kt
@@ -1,11 +1,10 @@
-package com.wafflestudio.snutt2.core.model.data.timetable
-
-import com.wafflestudio.snutt2.core.model.data.CourseBook
+package com.wafflestudio.snutt2.core.model.data
 
 data class SimpleTimeTable(
     val id: String,
     val courseBook: CourseBook,
     val title: String,
+    val updatedAt: String,
     val totalCredit: Long,
     val isPrimary: Boolean,
 )
