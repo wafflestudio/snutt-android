372f340d1b70dc2efd7cc8c0e5d6437236d3cb7a
plgafhd <plgafhd@snu.ac.kr>
2024-07-20 20:04:43 +0900

    pretendard 적용

--- app/src/main/java/com/wafflestudio/snutt2/ui/Typo.kt
+++ app/src/main/java/com/wafflestudio/snutt2/ui/Typo.kt
@@ -16,64 +16,76 @@ private val SpoqaHanSans = FontFamily(
     Font(R.font.spoqa_han_sans_bold, FontWeight.Bold),
 )
 
+private val Pretendard = FontFamily(
+    Font(R.font.pretendard_thin, FontWeight.Thin),
+    Font(R.font.pretendard_extralight, FontWeight.ExtraLight),
+    Font(R.font.pretendard_light, FontWeight.Light),
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_extrabold, FontWeight.ExtraBold),
+    Font(R.font.pretendard_black, FontWeight.Black),
+)
+
 val SNUTTTypography @Composable get() = Typography(
-    defaultFontFamily = SpoqaHanSans,
+    defaultFontFamily = Pretendard,
     h1 = TextStyle(
         color = SNUTTColors.Black900,
         fontSize = 22.sp,
-        fontFamily = SpoqaHanSans,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Bold,
     ),
     h2 = TextStyle(
         color = SNUTTColors.Black900,
         fontSize = 18.sp,
-        fontFamily = SpoqaHanSans,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Bold,
     ),
     h3 = TextStyle(
         color = SNUTTColors.Black900,
         fontSize = 16.sp,
-        fontFamily = SpoqaHanSans,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Bold,
     ),
     h4 = TextStyle(
         color = SNUTTColors.Black900,
         fontSize = 14.sp,
-        fontFamily = SpoqaHanSans,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Bold,
     ),
     h5 = TextStyle(
         color = SNUTTColors.Black900,
         fontSize = 12.sp,
-        fontFamily = SpoqaHanSans,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Bold,
     ),
     subtitle1 = TextStyle(
         color = SNUTTColors.Gray200,
         fontSize = 17.sp,
-        fontFamily = SpoqaHanSans,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Medium,
     ),
     subtitle2 = TextStyle(
         color = SNUTTColors.Gray200,
         fontSize = 14.sp,
-        fontFamily = SpoqaHanSans,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Medium,
     ),
     button = TextStyle(
         color = SNUTTColors.Black900,
         fontSize = 16.sp,
-        fontFamily = SpoqaHanSans,
+        fontFamily = Pretendard,
     ),
     body1 = TextStyle(
         color = SNUTTColors.Black900,
         fontSize = 14.sp,
-        fontFamily = SpoqaHanSans,
+        fontFamily = Pretendard,
     ),
     body2 = TextStyle(
         color = SNUTTColors.Black900,
         fontSize = 12.sp,
-        fontFamily = SpoqaHanSans,
+        fontFamily = Pretendard,
     ),
 )
 
