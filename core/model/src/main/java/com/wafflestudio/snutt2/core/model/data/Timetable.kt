fe6b51a7d987838ddeb3f1d1126471c9875d5d96
양주현 <didwngus01@naver.com>
2024-07-20 19:12:06 +0900

    core:model Timetable

--- core/model/src/main/java/com/wafflestudio/snutt2/core/model/data/Timetable.kt
+++ core/model/src/main/java/com/wafflestudio/snutt2/core/model/data/Timetable.kt
@@ -1,15 +1,14 @@
-package com.wafflestudio.snutt2.core.model.data.timetable
+package com.wafflestudio.snutt2.core.model.data
 
-import com.wafflestudio.snutt2.core.model.data.CourseBook
 import com.wafflestudio.snutt2.core.model.data.lecture.TimetableLecture
-import com.wafflestudio.snutt2.core.model.data.theme.TableTheme
 
-data class TimeTable(
+data class Timetable(
     val id: String,
     val courseBook: CourseBook,
     val title: String,
     val lectureList: List<TimetableLecture>,
+    val updateAt: String,
     val totalCredit: Long,
-    val theme: TableTheme,
     val isPrimary: Boolean,
+    val theme: Theme,
 )
