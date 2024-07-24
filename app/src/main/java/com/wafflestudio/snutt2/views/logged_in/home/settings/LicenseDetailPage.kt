30df773e49f571a24fdafbce3050d9fcc402bd71
plgafhd <plgafhd@snu.ac.kr>
2024-07-25 03:58:05 +0900

    ktlintformat

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/LicenseDetailPage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/LicenseDetailPage.kt
@@ -1,6 +1,5 @@
 package com.wafflestudio.snutt2.views.logged_in.home.settings
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,25 +9,15 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.wafflestudio.snutt2.R
 import com.wafflestudio.snutt2.components.compose.SimpleTopBar
-import com.wafflestudio.snutt2.components.compose.clicks
 import com.wafflestudio.snutt2.ui.SNUTTColors
 import com.wafflestudio.snutt2.ui.SNUTTTypography
-import com.wafflestudio.snutt2.views.LocalApiOnError
-import com.wafflestudio.snutt2.views.LocalApiOnProgress
 import com.wafflestudio.snutt2.views.LocalNavController
-import com.wafflestudio.snutt2.views.NavigationDestination
-import com.wafflestudio.snutt2.views.logged_in.home.showTitleChangeDialog
 
 @Composable
 fun LicenseDetailPage() {
@@ -82,14 +71,13 @@ fun LicenseDetailPage() {
         Text(
             text = name,
             style = SNUTTTypography.body2,
-            modifier = Modifier.padding(15.dp)
+            modifier = Modifier.padding(15.dp),
         )
 
         Text(
             text = content,
             style = SNUTTTypography.body2,
-            modifier = Modifier.padding(15.dp)
+            modifier = Modifier.padding(15.dp),
         )
     }
 }
-
