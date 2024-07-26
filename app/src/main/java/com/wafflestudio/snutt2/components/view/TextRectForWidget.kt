6e80e6c7a0c2c037cbc9e8f4e44f497bdf8ba0a9
plgafhd <plgafhd@snu.ac.kr>
2024-07-26 23:16:48 +0900

    위젯용 TextRect에서 필요없는 부분 삭제

--- app/src/main/java/com/wafflestudio/snutt2/components/view/TextRectForWidget.kt
+++ app/src/main/java/com/wafflestudio/snutt2/components/view/TextRectForWidget.kt
@@ -106,104 +106,6 @@ class TextRectForWidget(paint: Paint) {
     }
 
     /**
-     * Calculate height of text block and prepare to draw it in a single line.
-     *
-     * @param text - text to draw
-     * @param maxWidth - maximum width in pixels
-     * @param maxHeight - maximum height in pixels
-     * @returns height of text in pixels
-     */
-    fun prepareSingleLine(text: String, maxWidth: Int, maxHeight: Int): Int {
-        paint!!.getTextBounds(text, 0, text.length, bounds)
-        if (bounds.width() == 0) return 0
-        if (bounds.width() <= maxWidth) {
-            val lineHeight = -metrics!!.ascent + metrics!!.descent + metrics!!.leading
-            if (lineHeight > maxHeight) {
-                wasCut = true
-                return 0
-            }
-            wasCut = false
-            lines = 1
-            starts[0] = 0
-            stops[0] = text.length
-            textHeight = lineHeight
-            return textHeight
-        }
-
-        lines = 0
-        textHeight = 0
-        this.text = text
-        wasCut = true
-
-        // get maximum number of characters in one line
-        paint!!.getTextBounds("i", 0, 1, bounds)
-        val maximumInLine = maxWidth / bounds.width()
-        val length = text.length
-        if (length > 0) {
-            val lineHeight = -metrics!!.ascent + metrics!!.descent + metrics!!.leading
-            var start = 0
-            var stop = if (maximumInLine > length) length else maximumInLine
-            // stop = min(length,maximuminline);
-            while (true) {
-                // skip LF and spaces
-                while (start < length) {
-                    val ch = text[start]
-                    if (ch != '\n' && ch != '\r' && ch != '\t' && ch != ' ') break
-                    ++start
-                }
-                var o = stop + 1
-                while (stop < o && stop > start) {
-                    o = stop
-                    var lowest = text.indexOf("\n", start)
-                    // 강의명만 생각
-                    paint!!.getTextBounds("$text...", start, stop + 3, bounds)
-                    if (start <= lowest && lowest < stop || bounds.width() > maxWidth) // start ~ stop 까지 한 줄에 넣으려고 하는데 한 줄에 넘치면
-                        {
-                            --stop
-                            if (lowest < start || lowest > stop) {
-                                val blank = text.lastIndexOf(" ", stop)
-                                if (blank > start) lowest = blank
-                            }
-                            if (start <= lowest && lowest <= stop) {
-                                val ch = text[stop]
-                                if (ch != '\n' && ch != ' ') ++lowest
-                                stop = lowest
-                            }
-                            continue
-                        }
-                    break
-                }
-                if (start >= stop) break
-                var minus = 0
-
-                // cut off lf or space
-                if (stop < length) {
-                    val ch = text[stop - 1]
-                    if (ch == '\n' ||
-                        ch == ' '
-                    ) {
-                        minus = 1
-                    }
-                }
-                if (textHeight + lineHeight > maxHeight) {
-                    wasCut = true
-                    break
-                }
-                starts[lines] = start
-                stops[lines] = stop - minus + 3
-                if (++lines > MAX_LINES) {
-                    wasCut = true
-                    break
-                }
-                textHeight += lineHeight
-                if (stop >= length) break
-                break
-            }
-        }
-        return textHeight
-    }
-
-    /**
      * Draw prepared text at given position.
      *
      * @param canvas - canvas to draw text into
@@ -238,15 +140,6 @@ class TextRectForWidget(paint: Paint) {
             canvas.drawText(t, leftResult.toFloat(), y.toFloat(), paint!!)
             y += after
         }
-    }
-
-    /** Returns true if text was cut to fit into the maximum height  */
-    fun wasCut(): Boolean {
-        return wasCut
-    }
-
-    fun getLeading(): Int {
-        return metrics?.leading ?: 0
     }
 
     companion object {
