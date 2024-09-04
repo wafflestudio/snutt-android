c3242295b47c2ae88ed6a1e1a175d8b4a9001e2c
eastshine2741 <eastshine@snu.ac.kr>
2024-09-04 21:00:44 +0900

    정렬기준 다중 선택 불가능하도록 설정

--- app/src/main/java/com/wafflestudio/snutt2/model/TagType.kt
+++ app/src/main/java/com/wafflestudio/snutt2/model/TagType.kt
@@ -3,6 +3,13 @@ package com.wafflestudio.snutt2.model
 /**
  * Created by makesource on 2017. 8. 26..
  */
-enum class TagType {
-    CLASSIFICATION, DEPARTMENT, ACADEMIC_YEAR, CREDIT, TIME, CATEGORY, SORT_CRITERIA, ETC
+sealed class TagType(val isExclusive: Boolean) {
+    data object CLASSIFICATION : TagType(false)
+    data object DEPARTMENT : TagType(false)
+    data object ACADEMIC_YEAR : TagType(false)
+    data object CREDIT : TagType(false)
+    data object TIME : TagType(false)
+    data object CATEGORY : TagType(false)
+    data object SORT_CRITERIA : TagType(true)
+    data object ETC : TagType(false)
 }
