97ea588939801a2b752cb16f978ebcb06ac82209
plgafhd <plgafhd@snu.ac.kr>
2024-09-09 00:25:56 +0900

    lint

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/vacancy_noti/VacancyListItem.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/vacancy_noti/VacancyListItem.kt
@@ -13,7 +13,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
