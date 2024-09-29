8ce337e129fffc33fb2233a2fa97fbbcf52246a3
plgafhd <plgafhd@snu.ac.kr>
2024-09-29 14:12:18 +0900

    분기 처리

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_out/ImportantNoticePage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_out/ImportantNoticePage.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -21,21 +22,48 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.lifecycleScope
 import com.wafflestudio.snutt2.R
 import com.wafflestudio.snutt2.ui.SNUTTColors
 import com.wafflestudio.snutt2.ui.SNUTTTypography
 import com.wafflestudio.snutt2.views.LocalNavController
 import com.wafflestudio.snutt2.views.NavigationDestination
+import com.wafflestudio.snutt2.views.logged_in.home.HomeViewModel
+import com.wafflestudio.snutt2.views.logged_in.home.settings.UserViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun ImportantNoticePage() {
     val navController = LocalNavController.current
+    val coroutineScope = rememberCoroutineScope()
 
     val importantNoticeViewModel = hiltViewModel<ImportantNoticeViewModel>()
+    val userViewModel = hiltViewModel<UserViewModel>()
+    val homeViewModel = hiltViewModel<HomeViewModel>()
     val importantNotice = importantNoticeViewModel.importantNotice.collectAsState()
 
     LaunchedEffect(Unit) {
-        importantNoticeViewModel.getConfigs()
+        try {
+            importantNoticeViewModel.getConfigs()
+        } catch (e: Exception) {
+            val token = userViewModel.accessToken.value
+            coroutineScope.launch {
+                if (token.isNotEmpty()) {
+                    homeViewModel.refreshData()
+                    navController.navigate(NavigationDestination.Home) {
+                        popUpTo(NavigationDestination.ImportantNotice) {
+                            inclusive = true
+                        }
+                    }
+                } else {
+                    navController.navigate(NavigationDestination.Tutorial) {
+                        popUpTo(NavigationDestination.ImportantNotice) {
+                            inclusive = true
+                        }
+                    }
+                }
+            }
+        }
     }
 
     Column(
