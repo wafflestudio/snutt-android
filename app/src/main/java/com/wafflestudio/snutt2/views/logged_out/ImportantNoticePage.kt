b35ac48768f1b3affd202eeb4bd4fa700cf14f83
plgafhd <plgafhd@snu.ac.kr>
2024-09-29 16:01:58 +0900

    디자인 반영

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_out/ImportantNoticePage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_out/ImportantNoticePage.kt
@@ -4,12 +4,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.FloatingActionButtonDefaults
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -19,11 +24,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.wafflestudio.snutt2.R
+import com.wafflestudio.snutt2.components.compose.clicks
 import com.wafflestudio.snutt2.ui.SNUTTColors
 import com.wafflestudio.snutt2.ui.SNUTTTypography
 import com.wafflestudio.snutt2.views.LocalNavController
@@ -42,7 +49,7 @@ fun ImportantNoticePage(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(vertical = 12.dp, horizontal = 36.dp)
+            .padding(vertical = 12.dp, horizontal = 48.dp)
             .background(SNUTTColors.White900),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -50,7 +57,7 @@ fun ImportantNoticePage(
         Image(
             painter = painterResource(id = R.drawable.ic_cat_retry),
             contentDescription = stringResource(R.string.sign_in_logo_title),
-            modifier = Modifier.width(48.dp),
+            modifier = Modifier.width(55.dp),
         )
 
         Spacer(modifier = Modifier.height(32.dp))
@@ -72,25 +79,26 @@ fun ImportantNoticePage(
             textAlign = TextAlign.Center,
         )
 
-        Spacer(modifier = Modifier.height(32.dp))
+        Spacer(modifier = Modifier.height(20.dp))
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.clickable {
-                navController.navigate(NavigationDestination.AppReport)
-            },
+        Surface(
+            modifier = Modifier
+                .size(121.dp, 33.dp)
+                .clicks {
+                    navController.navigate(NavigationDestination.AppReport)
+                },
+            shape = RoundedCornerShape(18.dp),
+            color = SNUTTColors.SNUTTVacancy,
         ) {
-            Text(
-                text = "문의사항 접수",
-                style = SNUTTTypography.body1,
-            )
-
-            Spacer(modifier = Modifier.height(2.dp))
-
-            Text(
-                text = "snutt@wafflestudio.com",
-                style = SNUTTTypography.body1.copy(color = SNUTTColors.Blue),
-            )
+            Box(
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(
+                    text = stringResource(R.string.send_report),
+                    style = SNUTTTypography.h4.copy(color = SNUTTColors.AllWhite, fontWeight = FontWeight.SemiBold),
+                    maxLines = 1,
+                )
+            }
         }
     }
 }
