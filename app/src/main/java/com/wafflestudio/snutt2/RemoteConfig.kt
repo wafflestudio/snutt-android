c4a56674183a3daefd1f2c234b5bc7d3055c2375
eastshine2741 <eastshine@snu.ac.kr>
2024-10-09 18:27:34 +0900

    lint

--- app/src/main/java/com/wafflestudio/snutt2/RemoteConfig.kt
+++ app/src/main/java/com/wafflestudio/snutt2/RemoteConfig.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
