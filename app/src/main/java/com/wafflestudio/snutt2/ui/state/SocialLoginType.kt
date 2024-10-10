76c917caf35438d6719ce652f3f9da074af10500
plgafhd <plgafhd@snu.ac.kr>
2024-10-11 01:13:04 +0900

    TutorialPage의 LaunchedEffect 함수들 통합

--- app/src/main/java/com/wafflestudio/snutt2/ui/state/SocialLoginType.kt
+++ app/src/main/java/com/wafflestudio/snutt2/ui/state/SocialLoginType.kt
@@ -5,3 +5,11 @@ enum class SocialLoginType {
     KAKAO,
     GOOGLE,
 }
+
+fun SocialLoginType.getString(): String {
+    return when (this) {
+        SocialLoginType.FACEBOOK -> "페이스북"
+        SocialLoginType.GOOGLE -> "구글"
+        SocialLoginType.KAKAO -> "카카오"
+    }
+}
