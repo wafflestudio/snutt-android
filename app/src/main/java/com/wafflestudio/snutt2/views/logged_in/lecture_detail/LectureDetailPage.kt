9c26ecf21d6cb94068b1853efc7f4214c5316da1
이현도 <141345525+plgafhd@users.noreply.github.com>
2024-10-24 02:43:36 +0900

    LectureDetail에서 보여주는 ClassTime Text Format fix (#364)

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/lecture_detail/LectureDetailPage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/lecture_detail/LectureDetailPage.kt
@@ -481,7 +481,7 @@ fun LectureDetailPage(
                     )
                     editingLectureDetail.class_time_json.forEachIndexed { idx, classTime ->
                         LectureDetailTimeAndLocation(
-                            timeText = SNUTTStringUtils.getClassTimeText(classTime),
+                            timeText = SNUTTStringUtils.getSingleClassTimeText(classTime),
                             locationText = classTime.place,
                             editTime = {
                                 bottomSheet.setSheetContent {
