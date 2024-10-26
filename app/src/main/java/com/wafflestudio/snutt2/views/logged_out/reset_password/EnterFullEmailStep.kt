db751462b024a0d8794b42458931b2601cd494a4
양주현 <didwngus01@naver.com>
2024-10-26 20:48:33 +0900

    feat: Step 2 - 전체 이메일 입력 화면 개선
    - 비슷하게, 포커스 처리 + 커서 처리 + keyboardActions 처리 + imePadding

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_out/reset_password/EnterFullEmailStep.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_out/reset_password/EnterFullEmailStep.kt
@@ -6,11 +6,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,14 +23,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.wafflestudio.snutt2.R
 import com.wafflestudio.snutt2.components.compose.EditText
+import com.wafflestudio.snutt2.components.compose.EditTextFieldValue
 import com.wafflestudio.snutt2.components.compose.WebViewStyleButton
 import com.wafflestudio.snutt2.components.compose.clicks
 import com.wafflestudio.snutt2.lib.android.toast
@@ -34,7 +41,6 @@ import com.wafflestudio.snutt2.lib.data.SNUTTStringUtils.isEmailInvalid
 import com.wafflestudio.snutt2.ui.SNUTTColors
 import com.wafflestudio.snutt2.ui.SNUTTTypography
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun EnterFullEmailStep(
     userId: String,
@@ -43,27 +49,42 @@ fun EnterFullEmailStep(
     notMyEmail: () -> Unit,
     onSubmitFullEmail: (String) -> Unit,
 ) {
-    val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
     val context = LocalContext.current
 
-    var emailField by remember { mutableStateOf(fullEmail) }
+    var emailField by remember {
+        mutableStateOf(
+            TextFieldValue(
+                text = fullEmail,
+                selection = TextRange(fullEmail.length), // 초기 커서를 텍스트 끝으로 설정
+            ),
+        )
+    }
     val buttonEnabled by remember {
         derivedStateOf {
-            !emailField.isEmailInvalid()
+            !emailField.text.isEmailInvalid()
         }
     }
 
-    val sendIdAndRequestMaskedEmail = {
-        if (emailField.isEmpty()) {
-            context.toast(context.getString(R.string.find_password_enter_id_hint))
-        } else {
-            onSubmitFullEmail(emailField)
+    val sendIdAndRequestMaskedEmail: () -> Unit = {
+        if (buttonEnabled) {
+            if (emailField.text.isEmpty()) {
+                context.toast(context.getString(R.string.find_password_enter_id_hint))
+            } else {
+                onSubmitFullEmail(emailField.text)
+            }
         }
+    }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
     }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .imePadding()
             .padding(vertical = 44.dp, horizontal = 20.dp),
     ) {
         Text(
@@ -97,16 +118,16 @@ fun EnterFullEmailStep(
 
         Spacer(modifier = Modifier.height(12.dp))
 
-        EditText(
-            modifier = Modifier.fillMaxWidth(),
+        EditTextFieldValue(
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(focusRequester),
             value = emailField,
             onValueChange = { emailField = it },
             hint = stringResource(R.string.find_password_check_email_enter_full_email_hint),
             keyboardActions = KeyboardActions(
-                onNext = {
-                    focusManager.moveFocus(
-                        FocusDirection.Enter,
-                    )
+                onDone = {
+                    sendIdAndRequestMaskedEmail()
                 },
             ),
             keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
