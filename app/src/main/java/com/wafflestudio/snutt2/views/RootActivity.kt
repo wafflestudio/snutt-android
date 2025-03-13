5f4fe4265600a693a30d6124f0a91b1cc1b5dbbd
eastshine2741 <eastshine@snu.ac.kr>
2025-03-14 02:03:35 +0900

    수정사항넣기

--- app/src/main/java/com/wafflestudio/snutt2/views/RootActivity.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/RootActivity.kt
@@ -83,6 +83,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+// 뭔가수정사항넣기
 @ExperimentalAnimationApi
 @AndroidEntryPoint
 class RootActivity : AppCompatActivity() {
