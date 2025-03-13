d3487246b7a52c7874b26e09b4a90df8dca870e0
eastshine2741 <eastshine@snu.ac.kr>
2025-03-14 02:35:28 +0900

    두 줄 수정하고

--- app/src/main/java/com/wafflestudio/snutt2/lib/network/dto/core/LectureReviewDto.kt
+++ app/src/main/java/com/wafflestudio/snutt2/lib/network/dto/core/LectureReviewDto.kt
@@ -15,6 +15,8 @@ data class LectureReviewDto(
     val ratingDisplayText get() = rating?.times(10)?.toInt()?.div(10.0)?.toString() ?: "--"
     val displayText get() = "$ratingDisplayText (${reviewCount ?: 0})"
 
+    // asdf
+    // asdfg
     fun getReviewUrl(context: Context): String? { // DTO와 ui model 분리 후 ui model으로 옮기기
         return id.let {
             context.getString(R.string.review_base_url) + "/detail?id=$it"
