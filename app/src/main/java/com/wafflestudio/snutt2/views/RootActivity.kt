c8ce3fe45b89ac6194cbde784e1d4035aa3cb4a6
eastshine2741 <eastshine@snu.ac.kr>
2025-03-14 02:04:50 +0900

    gpt 3.5 turbo는 max token이 4096이라네용

--- app/src/main/java/com/wafflestudio/snutt2/views/RootActivity.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/RootActivity.kt
@@ -83,7 +83,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-// 뭔가수정사항넣기
+// 뭔가수정사항넣기2
 @ExperimentalAnimationApi
 @AndroidEntryPoint
 class RootActivity : AppCompatActivity() {
