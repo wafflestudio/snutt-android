c93fdf56dc51841e19641fd1cabc147c50023dcd
qdrptd <qdrptd@gmail.com>
2025-04-27 15:04:15 +0900

    add/학기 선택 버튼

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/LectureDiaryPage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/LectureDiaryPage.kt
@@ -2,26 +2,60 @@ package com.wafflestudio.snutt2.views.logged_in.home.settings
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.wafflestudio.snutt2.components.compose.*
 import com.wafflestudio.snutt2.ui.SNUTTColors
+import com.wafflestudio.snutt2.ui.SNUTTTypography
 import com.wafflestudio.snutt2.views.*
 
 @Composable
 fun LectureDiaryPage() {
-    val semesters = listOf("24-1", "23-겨울")
+    val semesters = listOf("24-1", "23-겨울", "23-2", "23-1", "23-겨울")
     Box {
         Column(
             modifier = Modifier.background(SNUTTColors.White900),
         ) {
-            TopBar()
-            Row(
-                modifier = Modifier.padding(top = 16.dp, bottom = 4.dp),
+            TopBar(
+                title = { Text("") },
+                navigationIcon = {
+                    ArrowBackIcon(
+                        modifier = Modifier.size(30.dp),
+                        colorFilter = ColorFilter.tint(SNUTTColors.Black900),
+                    )
+                },
+            )
+            LazyRow(
+                modifier = Modifier.padding(top = 20.dp, bottom = 12.dp),
+                contentPadding = PaddingValues(horizontal = 20.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
             ) {
+                items(semesters) { semester ->
+                    Box(
+                        modifier = Modifier
+                            .background(SNUTTColors.SNUTTTheme, RoundedCornerShape(50))
+                            .padding(horizontal = 24.dp)
+                            .height(34.dp),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text(semester, color = SNUTTColors.White900, style = SNUTTTypography.subtitle1.copy( fontWeight = FontWeight.SemiBold))
+                    }
+
+                }
             }
+
         }
     }
 }
