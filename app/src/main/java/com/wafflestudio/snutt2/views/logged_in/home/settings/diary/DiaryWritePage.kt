428dd04f69bb1fe2c759f8061693737581647558
qdrptd <qdrptd@gmail.com>
2025-05-24 15:13:30 +0900

    안 쓰는 import 삭제

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/diary/DiaryWritePage.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/diary/DiaryWritePage.kt
@@ -3,8 +3,6 @@ package com.wafflestudio.snutt2.views.logged_in.home.settings.diary
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
