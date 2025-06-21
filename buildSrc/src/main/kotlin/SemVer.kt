580e522c77cc7c1e94d478053802a7c26eaa814a
plgafhd <plgafhd@snu.ac.kr>
2025-06-21 15:36:35 +0900

    버전 코드 계산 방식 변경

--- buildSrc/src/main/kotlin/SemVer.kt
+++ buildSrc/src/main/kotlin/SemVer.kt
@@ -6,10 +6,12 @@ object SemVer {
         val major = semVerRegex.find(semanticVersion)?.groupValues?.get(1)?.toLongOrNull() ?: 0L
         val minor = semVerRegex.find(semanticVersion)?.groupValues?.get(2)?.toLongOrNull() ?: 0L
         val patch = semVerRegex.find(semanticVersion)?.groupValues?.get(3)?.toLongOrNull() ?: 0L
+        // group 4는 "rc-N" 전체이고, group 5가 N이다.
+        val rc = semVerRegex.find(semanticVersion)?.groupValues?.get(5)?.toLongOrNull() ?: 99L
 
-        return listOf(major, minor, patch)
+        return listOf(major, minor, patch, rc)
             .fold(0L) { acc, next ->
                 acc * 100 + next
-            } + 2010000000L
+            } + 2100000000L
     }
 }
