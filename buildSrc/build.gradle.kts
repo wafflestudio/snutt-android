efe580fa719a5339189ced209aa9475f0a81a011
이현도 <141345525+plgafhd@users.noreply.github.com>
2025-06-23 01:12:22 +0900

    버전 코드 계산 방식 변경 (#443)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,2 +1,20 @@
 plugins { `kotlin-dsl` }
-repositories { mavenCentral() }
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    val junitJupiterVersion = "5.10.3"
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("PASSED", "SKIPPED", "FAILED", "STANDARD_OUT", "STANDARD_ERROR")
+        showStandardStreams = true
+    }
+}
