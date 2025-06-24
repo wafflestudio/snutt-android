88f41cd0ae5ebb827227df5871356d7dca596471
plgafhd <plgafhd@snu.ac.kr>
2025-06-25 03:07:02 +0900

    TestScreen 관련 살짝 수정

--- app/src/main/java/com/wafflestudio/snutt2/views/RootActivity.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/RootActivity.kt
@@ -464,8 +464,12 @@ class RootActivity : AppCompatActivity() {
         if (BuildConfig.DEBUG) {
             composableAnimated<NavigationDestination.Test> {
                 TestRoute(
-                    onNavigateBack = { navController.popBackStack() },
-                    onNavigateOnBoard = { navController.navigateAsOrigin(NavigationDestination.Onboard) },
+                    onNavigateBack = {
+                        if (navController.currentDestination?.hasRoute(NavigationDestination.Test::class) == true) {
+                            navController.popBackStack()
+                        }
+                    },
+                    onNavigateOnboard = { navController.navigateAsOrigin(NavigationDestination.Onboard) },
                 )
             }
         }
