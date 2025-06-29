ffa92dfa20913ae382631a403a83c793c0a5403b
qdrptd <99246971+qdrptd@users.noreply.github.com>
2025-06-29 17:10:26 +0900

    일기 쓰기 페이지 Repository + ViewModel (#447)

---
+++ app/src/main/java/com/wafflestudio/snutt2/data/lecture_diary/DiaryRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.wafflestudio.snutt2.data.lecture_diary
+
+import com.wafflestudio.snutt2.domainmodel.DiaryWrite
+import com.wafflestudio.snutt2.domainmodel.preview.DiaryPreviewData
+import com.wafflestudio.snutt2.lib.network.Result
+import com.wafflestudio.snutt2.lib.network.SNUTTRestApi
+import com.wafflestudio.snutt2.lib.network.Unknown
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class DiaryRepositoryImpl @Inject constructor(
+    private val api: SNUTTRestApi,
+) :
+    DiaryRepository {
+    override fun getDiaryWriteInit(): Result<DiaryWrite> {
+        return Result.Success(DiaryPreviewData.diaryWriteInit)
+    }
+
+    override suspend fun saveDiaryWrite(diaryWriteData: DiaryWrite): Result<Unit> {
+        return Result.Fail(Unknown("")) // good
+    }
+}
--- app/src/main/java/com/wafflestudio/snutt2/domainmodel/preview/DiaryPreviewData.kt
+++ app/src/main/java/com/wafflestudio/snutt2/domainmodel/preview/DiaryPreviewData.kt
@@ -5,22 +5,25 @@ import com.wafflestudio.snutt2.domainmodel.DiaryWriteQuestion
 
 object DiaryPreviewData {
 
-    val diaryWritePreviewData =
-        DiaryWrite(
-            lectureName = "시각디자인기초",
-            questions = listOf(
-                DiaryWriteQuestion(
-                    question = "수강신청은 어땠나요?",
-                    options = listOf("널널했어요", "1픽 했어요", "2~3픽 했어요", "초안지 썼어요"),
-                ),
-                DiaryWriteQuestion(
-                    question = "교수님의 첫인상은 어땠나요?",
-                    options = listOf("두려워요", "두려워요", "유익했어요", "유익했어요"),
-                ),
-                DiaryWriteQuestion(
-                    question = "수업 끝까지 들을 것 같나요?",
-                    options = listOf("끝까지 들을 거에요", "모르겠어요", "드랍할 것 같아요"),
-                ),
-            ),
+    val diaryWriteQuestion =
+        DiaryWriteQuestion(
+            question = "수업 끝까지 들을 것 같나요?",
+            options = listOf("끝까지 들을 거에요", "모르겠어요", "드랍할 것 같아요"),
         )
+    val diaryWriteInit = DiaryWrite(
+        "알고리즘",
+        todayState = listOf(
+            false, true, true, false, false, false, false, false,
+        ),
+        questionsState = listOf(
+            listOf(true, false, false),
+            listOf(true, false, false),
+            listOf(false, true, false),
+        ),
+        moreText = "좋았어요 ㅇㅅㅇ",
+    )
+    val diaryWriteNewInit = DiaryWrite(
+        "알고리즘",
+        null, null, "",
+    )
 }
