14ca1b4317a6495552c5cfff9c50f98ef2946b67
qdrptd <99246971+qdrptd@users.noreply.github.com>
2025-07-07 12:21:15 +0900

    DiaryWritePage 수정 (#451)

--- app/src/main/java/com/wafflestudio/snutt2/views/NavigationDestination.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/NavigationDestination.kt
@@ -129,6 +129,10 @@ sealed interface NavigationDestination {
     data object LectureDiaryWrite : NavigationDestination
 
     @Serializable
+    @DeepLinkPath("lecture_diary_complete")
+    data class LectureDiaryComplete(val isCourseOver: Boolean) : NavigationDestination
+
+    @Serializable
     @DeepLinkPath("friends")
     data object Friends : NavigationDestination
 
