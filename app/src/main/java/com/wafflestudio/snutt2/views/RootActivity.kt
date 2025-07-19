4f6ba0f5de0577b036765a58ee1bcdcd2c8e3d37
이현도 <141345525+plgafhd@users.noreply.github.com>
2025-07-19 15:00:33 +0900

    UserConfigPage 리팩토링 (#448)

--- app/src/main/java/com/wafflestudio/snutt2/views/RootActivity.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/RootActivity.kt
@@ -431,7 +431,24 @@ class RootActivity : AppCompatActivity() {
         composableAnimated<NavigationDestination.ServiceInfo> { ServiceInfoPage() }
         composableAnimated<NavigationDestination.TeamInfo> { TeamInfoPage() }
         composableAnimated<NavigationDestination.TimeTableConfig> { TimetableConfigPage() }
-        composableAnimated<NavigationDestination.UserConfig> { UserConfigPage() }
+        composableAnimated<NavigationDestination.UserConfig> {
+            UserConfigRoute(
+                onNavigateBack = {
+                    if (navController.currentDestination?.hasRoute(NavigationDestination.UserConfig::class) == true) {
+                        navController.popBackStack()
+                    }
+                },
+                onNavigateOnboard = {
+                    navController.navigateAsOrigin(NavigationDestination.Onboard)
+                },
+                onNavigateChangeNickname = {
+                    navController.navigate(NavigationDestination.ChangeNickname)
+                },
+                onNavigateSocialLink = {
+                    navController.navigate(NavigationDestination.SocialLink)
+                },
+            )
+        }
         composableAnimated<NavigationDestination.ChangeNickname> { ChangeNicknamePage() }
         composableAnimated<NavigationDestination.SocialLink> { SocialLinkPage() }
         composableAnimated<NavigationDestination.PersonalInformationPolicy> { PersonalInformationPolicyPage() }
