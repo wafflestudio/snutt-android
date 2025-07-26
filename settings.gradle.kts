3a7c6f3c41858079199e3b8477edf3135b260978
양주현 <didwngus01@naver.com>
2025-07-27 05:22:21 +0900

    feat: 빌드캐시 테스트

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,3 +19,27 @@ dependencyResolutionManagement {
 
 rootProject.name = "snutt2"
 include(":app")
+
+plugins {
+    id("com.github.burrunan.s3-build-cache") version "1.9.3"
+}
+
+val isGithubAction = true //System.getenv("GITHUB_ACTIONS") == "true"
+
+buildCache {
+    // GITHUB ACTION 에서 수행된 경우가 아니면 Local Cache 사용
+    local {
+        isEnabled = isGithubAction.not()
+    }
+
+    // GITHUB ACTION 에서 수행되었으면 Remote Cache(S3) 사용
+    if (isGithubAction) {
+        remote<com.github.burrunan.s3cache.AwsS3BuildCache> {
+            region = "ap-northeast-2"
+            bucket = "snutt-android-build-cache"
+            prefix = "gradle/build/snutt-android/"
+            isPush = true
+            lookupDefaultAwsCredentials = true
+        }
+    }
+}
