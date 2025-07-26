7ee2c38205b33f4a590f8b057cb847c66ff246e7
양주현 <didwngus01@naver.com>
2025-07-27 07:34:03 +0900

    revert: s3 캐시 제거 테스트

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,18 +19,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "snutt2"
 include(":app")
-
-plugins {
-    id("com.github.burrunan.s3-build-cache") version "1.9.3"
-}
-
-buildCache {
-    // GITHUB ACTION 에서 수행되었으면 Remote Cache(S3) 사용
-    remote<com.github.burrunan.s3cache.AwsS3BuildCache> {
-        region = "ap-northeast-2"
-        bucket = "snutt-android-build-cache"
-        prefix = "gradle/build/snutt-android/"
-        isPush = true
-        lookupDefaultAwsCredentials = true
-    }
-}
