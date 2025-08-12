097152311c154d444339e1d7db4d867c66f251c6
이현도 <141345525+plgafhd@users.noreply.github.com>
2025-08-12 16:58:14 +0900

    강의 리마인더 Phase 1 (#463)

---
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/settings/LectureReminderViewModel.kt
@@ -0,0 +1,143 @@
+package com.wafflestudio.snutt2.views.logged_in.home.settings
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.wafflestudio.snutt2.data.tables.TableRepository
+import com.wafflestudio.snutt2.data.user.UserRepository
+import com.wafflestudio.snutt2.lib.debouncePerKey
+import com.wafflestudio.snutt2.lib.network.AuthError
+import com.wafflestudio.snutt2.lib.network.DisplayMessageResolver
+import com.wafflestudio.snutt2.lib.network.DomainError
+import com.wafflestudio.snutt2.lib.network.onFailure
+import com.wafflestudio.snutt2.lib.network.onSuccess
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LectureReminderViewModel @Inject constructor(
+    private val tableRepository: TableRepository,
+    private val userRepository: UserRepository,
+    private val displayMessageResolver: DisplayMessageResolver,
+) : ViewModel() {
+    private val _lectureReminderUiState = MutableStateFlow<LectureReminderUiState>(LectureReminderUiState.Loading)
+    val lectureReminderUiState = _lectureReminderUiState.asStateFlow()
+
+    private val _updateEvent = MutableSharedFlow<Pair<String, LectureWithReminderOption>>()
+
+    private val _lectureReminderUiEvent: MutableSharedFlow<LectureReminderUiEvent> = MutableSharedFlow(replay = 1)
+    val lectureReminderUiEvent = _lectureReminderUiEvent.asSharedFlow()
+
+    init {
+        loadInitialData()
+        handleUpdateEvents()
+    }
+
+    private fun loadInitialData() {
+        viewModelScope.launch {
+            tableRepository.getPrimaryTableLectureReminders()
+                .onSuccess { data ->
+                    _lectureReminderUiState.emit(LectureReminderUiState.Success(data.associateBy { it.lectureId }))
+                }
+                .onFailure {
+                    _lectureReminderUiState.emit(LectureReminderUiState.Error)
+                }
+        }
+    }
+
+    private fun handleUpdateEvents() {
+        viewModelScope.launch {
+            _updateEvent
+                .debouncePerKey(200L) { (lectureId, _) -> lectureId } // lectureId가 Key로 사용되어 lectureId가 서로 다른 변경은 debounce 없이 collect 한다.
+                .distinctUntilChanged()
+                .onEach { (lectureId, option) ->
+                    tableRepository.updateTableLectureReminders(lectureId, option)
+                        .onSuccess {
+                            _lectureReminderUiEvent.emit(
+                                LectureReminderUiEvent.ShowSnackBarByEvent(
+                                    when (option.lectureReminderOffset) {
+                                        LectureReminderOffset.NONE -> LectureReminderEvent.LECTURE_REMINDER_UPDATE_SUCCESS_NONE
+                                        LectureReminderOffset.TEN_MINUTES_BEFORE -> LectureReminderEvent.LECTURE_REMINDER_UPDATE_SUCCESS_TEN_MINUTES_BEFORE
+                                        LectureReminderOffset.AT_START_TIME -> LectureReminderEvent.LECTURE_REMINDER_UPDATE_SUCCESS_AT_START_TIME
+                                        LectureReminderOffset.TEN_MINUTES_AFTER -> LectureReminderEvent.LECTURE_REMINDER_UPDATE_SUCCESS_TEN_MINUTES_AFTER
+                                    },
+                                ),
+                            )
+                        }
+                        .onFailure { error ->
+                            handleLectureReminderError(error)
+                        }
+                }
+                .collect()
+        }
+    }
+
+    private suspend fun handleLectureReminderError(error: DomainError) {
+        val displayMessage = displayMessageResolver.getDisplayMessage(error)
+        when (error) {
+            is AuthError -> {
+                _lectureReminderUiEvent.emit(LectureReminderUiEvent.ShowToast(displayMessage))
+                userRepository.postForceLogout()
+                _lectureReminderUiEvent.emit(LectureReminderUiEvent.LoggedOut)
+            }
+            else -> {
+                _lectureReminderUiEvent.emit(LectureReminderUiEvent.ShowToast(displayMessage))
+            }
+        }
+    }
+
+    fun changeLectureReminderOption(lectureId: String, option: LectureWithReminderOption) {
+        _lectureReminderUiState.update { currentState ->
+            if (currentState is LectureReminderUiState.Success) {
+                currentState.copy(
+                    data = currentState.data.toMutableMap().apply {
+                        this[lectureId] = option
+                    },
+                )
+            } else {
+                currentState
+            }
+        }
+
+        viewModelScope.launch {
+            _updateEvent.emit(lectureId to option)
+        }
+    }
+}
+
+sealed interface LectureReminderUiState {
+    data object Loading : LectureReminderUiState
+    data object Error : LectureReminderUiState
+    data class Success(val data: Map<String, LectureWithReminderOption>) : LectureReminderUiState
+}
+
+data class LectureWithReminderOption(
+    val lectureId: String,
+    val lectureTitle: String,
+    val lectureReminderOffset: LectureReminderOffset,
+)
+
+enum class LectureReminderOffset {
+    NONE, TEN_MINUTES_BEFORE, AT_START_TIME, TEN_MINUTES_AFTER
+}
+
+sealed interface LectureReminderUiEvent {
+    data class ShowToast(val message: String) : LectureReminderUiEvent
+    data class ShowSnackBarByEvent(val event: LectureReminderEvent) : LectureReminderUiEvent
+    data object LoggedOut : LectureReminderUiEvent
+}
+
+enum class LectureReminderEvent {
+    LECTURE_REMINDER_UPDATE_SUCCESS_NONE,
+    LECTURE_REMINDER_UPDATE_SUCCESS_TEN_MINUTES_BEFORE,
+    LECTURE_REMINDER_UPDATE_SUCCESS_AT_START_TIME,
+    LECTURE_REMINDER_UPDATE_SUCCESS_TEN_MINUTES_AFTER,
+}
