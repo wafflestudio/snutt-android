bcc3c80c914978ff0edd1ac938c9016dca5c477f
qdrptd <qdrptd@gmail.com>
2025-08-21 23:05:35 +0900

    DomainError에 displayTitle 추가

--- app/src/main/java/com/wafflestudio/snutt2/lib/network/ExceptionMapper.kt
+++ app/src/main/java/com/wafflestudio/snutt2/lib/network/ExceptionMapper.kt
@@ -10,28 +10,29 @@ fun Exception.toDomainError(): DomainError {
     Timber.e(this)
 
     return when (this) {
-        is IOException -> NetworkDisconnect("")
-        is CancellationException -> Nothing("")
+        is IOException -> NetworkDisconnect("", "")
+        is CancellationException -> Nothing("", "")
         is ErrorParsedHttpException -> {
+            val displayTitle = this.errorDTO?.displayTitle ?: ""
             val displayMessage = this.errorDTO?.displayMessage ?: ""
             return when (this.errorDTO?.code) {
-                ErrorCode.SERVER_FAULT -> ServerFault(displayMessage)
-                ErrorCode.NO_ADMIN_PRIVILEGE -> NoAdminPrivilege(displayMessage)
-                ErrorCode.WRONG_API_KEY -> WrongApiKey(displayMessage)
-                ErrorCode.NO_USER_TOKEN -> NoUserToken(displayMessage)
-                ErrorCode.WRONG_USER_TOKEN -> WrongUserToken(displayMessage)
+                ErrorCode.SERVER_FAULT -> ServerFault(displayTitle, displayMessage)
+                ErrorCode.NO_ADMIN_PRIVILEGE -> NoAdminPrivilege(displayTitle, displayMessage)
+                ErrorCode.WRONG_API_KEY -> WrongApiKey(displayTitle, displayMessage)
+                ErrorCode.NO_USER_TOKEN -> NoUserToken(displayTitle, displayMessage)
+                ErrorCode.WRONG_USER_TOKEN -> WrongUserToken(displayTitle, displayMessage)
 
-                ErrorCode.INVALID_ID -> InvalidId(displayMessage)
-                ErrorCode.INVALID_PASSWORD -> InvalidPassword(displayMessage)
-                ErrorCode.DUPLICATE_ID -> DuplicateId(displayMessage)
-                ErrorCode.USED_EMAIL -> UsedEmail(displayMessage)
-                ErrorCode.WRONG_PASSWORD -> WrongPassword(displayMessage)
-                else -> Unknown(displayMessage)
+                ErrorCode.INVALID_ID -> InvalidId(displayTitle, displayMessage)
+                ErrorCode.INVALID_PASSWORD -> InvalidPassword(displayTitle, displayMessage)
+                ErrorCode.DUPLICATE_ID -> DuplicateId(displayTitle, displayMessage)
+                ErrorCode.USED_EMAIL -> UsedEmail(displayTitle, displayMessage)
+                ErrorCode.WRONG_PASSWORD -> WrongPassword(displayTitle, displayMessage)
+                else -> Unknown(displayTitle, displayMessage)
             }
         }
         else -> {
             FirebaseCrashlytics.getInstance().recordException(this)
-            Unknown("")
+            Unknown("", "")
         }
     }
 }
