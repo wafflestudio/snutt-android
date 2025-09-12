0c286044201cdb2e0139a1db34867dc30f31329b
qdrptd <99246971+qdrptd@users.noreply.github.com>
2025-09-12 22:50:18 +0900

    errorTitle 추가 (#477)

--- app/src/main/java/com/wafflestudio/snutt2/lib/network/DisplayMessageResolver.kt
+++ app/src/main/java/com/wafflestudio/snutt2/lib/network/DisplayMessageResolver.kt
@@ -1,5 +1,6 @@
 package com.wafflestudio.snutt2.lib.network
 
 interface DisplayMessageResolver {
+    fun getDisplayTitle(error: DomainError): String
     fun getDisplayMessage(error: DomainError): String
 }
