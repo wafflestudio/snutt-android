5ce0f7dcc7a77c9193f039308fe38f3ff33b5086
qdrptd <99246971+qdrptd@users.noreply.github.com>
2025-09-12 22:50:18 +0900

    errorTitle 추가 (#477)

--- app/src/main/java/com/wafflestudio/snutt2/lib/network/ApiOnError.kt
+++ app/src/main/java/com/wafflestudio/snutt2/lib/network/ApiOnError.kt
@@ -121,6 +121,7 @@ object ErrorCode {
 data class ErrorDTO(
     @Json(name = "errcode") val code: Int? = null,
     @Json(name = "message") val message: String? = null,
+    @Json(name = "displayTitle") val displayTitle: String? = null,
     @Json(name = "displayMessage") val displayMessage: String? = null,
     @Json(name = "ext") val ext: Map<String, String>? = null,
 )
