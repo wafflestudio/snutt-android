039069764fea97ab9b230347f17481f2ce5ab813
양주현 <didwngus01@naver.com>
2025-09-27 02:30:21 +0900

    feat: storage의 시간표 목록 구독해서 상태 만들기

--- app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/drawer/refactor/HomeDrawerViewModel.kt
+++ app/src/main/java/com/wafflestudio/snutt2/views/logged_in/home/drawer/refactor/HomeDrawerViewModel.kt
@@ -32,6 +32,49 @@ class HomeDrawerViewModel @Inject constructor(
         )
 
     val uiState = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            courseBookRepository.getCourseBookNew()
+                .onFailure {
+                    // TODO
+                    return@launch
+                }
+                // 콜백지옥인데..
+                .onSuccess { coursebookList ->
+                    tableRepository.tableSummaryList.collect { tableSummaryList ->
+                        // 유저의 전체 시간표 목록을 학기 별로 그룹핑 한 뒤 학기 순으로 정렬
+                        val tableSummariesOfEachCourseBook = tableSummaryList.groupBy {
+                            it.courseBook
+                        }.toMutableMap()
+
+                        // 가장 최신 학기는 생성된 시간표가 없어도 서랍에서 보여주기
+                        val mostRecentCourseBook = coursebookList.first()
+                        if (tableSummariesOfEachCourseBook.containsKey(mostRecentCourseBook)
+                                .not()
+                        ) {
+                            tableSummariesOfEachCourseBook[mostRecentCourseBook] = emptyList()
+                        }
+
+                        _uiState.value = _uiState.value.copy(
+                            courseBookDrawerItemList = tableSummariesOfEachCourseBook
+                                .toList()
+                                .sortedBy { (coursebook, _) -> coursebook }
+                                .map { (courseBook, tableSummaries) ->
+                                    CoursebookDrawerItem(
+                                        courseBook = courseBook,
+                                        showNewCoursebookDot = (courseBook == mostRecentCourseBook) && tableSummaries.isEmpty(),
+                                        tableList = tableSummaries
+                                    ).toDataWithState(
+                                        // TODO: 현재 선택된 시간표가 속한 coursebook 은 초기 펼침 상태여야 함
+                                        false
+                                    )
+                                }
+                        )
+                    }
+                }
+        }
+    }
 }
 
 data class HomeDrawerUiState(
