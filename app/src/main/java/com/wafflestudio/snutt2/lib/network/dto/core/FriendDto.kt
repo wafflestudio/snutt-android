8a2931ad3fecd8dace37e46ece5de0913b3e7407
양주현 <didwngus01@naver.com>
2025-11-09 02:48:38 +0900

    feat: 신규 도메인 모델 및 신규 서버 DTO 변환 함수 추가

--- app/src/main/java/com/wafflestudio/snutt2/lib/network/dto/core/FriendDto.kt
+++ app/src/main/java/com/wafflestudio/snutt2/lib/network/dto/core/FriendDto.kt
@@ -2,6 +2,8 @@ package com.wafflestudio.snutt2.lib.network.dto.core
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import com.wafflestudio.snutt2.domainmodel.Friend
+import com.wafflestudio.snutt2.domainmodel.Nickname
 
 // 신규
 @JsonClass(generateAdapter = true)
@@ -11,4 +13,17 @@ data class FriendDto(
     @param:Json(name = "displayName") val displayName: String?,
     @param:Json(name = "nickname") val nickname: NicknameDto,
     @param:Json(name = "createdAt") val createdAt: String,
-)
+) {
+    fun toDomainModel(): Friend {
+        return Friend(
+            id = id,
+            userId = userId,
+            displayName = displayName,
+            nickname = Nickname(
+                nickname = nickname.nickname,
+                tag = nickname.tag,
+            ),
+            createdAt = createdAt,
+        )
+    }
+}
