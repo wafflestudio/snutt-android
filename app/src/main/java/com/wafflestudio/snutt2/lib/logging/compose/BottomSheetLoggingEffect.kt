fb1f72fe85ec0f79530f3c422b24220140d94589
양주현 <didwngus01@naver.com>
2026-03-29 02:49:50 +0900

    chore: 필요없는 헬퍼 파일 제거

--- app/src/main/java/com/wafflestudio/snutt2/lib/logging/compose/BottomSheetLoggingEffect.kt
+++
@@ -1,5 +0,0 @@
-package com.wafflestudio.snutt2.lib.logging.compose
-
-// 구 BottomSheet 방식에 의존하던 BottomSheetLoggingEffect 는 제거됨.
-// 신규 바텀시트 방식에서는 각 BottomSheetLayout의 sheetContent when 분기에서
-// LaunchedEffect(Unit) { analyticsLogger.logScreen(...) } 패턴으로 개별 로깅.
