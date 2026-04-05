95c7ed107bff763fa0286c0a07e693544f765927
양주현 <didwngus01@naver.com>
2026-04-05 17:28:14 +0900

    test: Fake 구현체 준비

--- app/src/test/java/com/wafflestudio/snutt2/fake/FakeUserRepository.kt
+++ app/src/test/java/com/wafflestudio/snutt2/fake/FakeUserRepository.kt
@@ -28,6 +28,42 @@ class FakeUserRepository : UserRepository {
     var postFeedbackCalledWith: Pair<String, String>? = null
         private set
 
+    var checkEmailByIdResult: Result<String> = Result.Success("")
+    var checkEmailByIdCalledWith: String? = null
+        private set
+
+    var sendPwResetCodeToEmailResult: Result<Unit> = Result.Success(Unit)
+    var sendPwResetCodeToEmailCalledWith: String? = null
+        private set
+
+    var verifyPwResetCodeResult: Result<Unit> = Result.Success(Unit)
+    var verifyPwResetCodeCalledWith: Pair<String, String>? = null
+        private set
+
+    var resetPasswordResult: Result<Unit> = Result.Success(Unit)
+    var resetPasswordCalledWith: Triple<String, String, String>? = null
+        private set
+
+    var putUserPasswordResult: Result<Unit> = Result.Success(Unit)
+    var putUserPasswordCalledWith: Pair<String, String>? = null
+        private set
+
+    var postUserPasswordResult: Result<Unit> = Result.Success(Unit)
+    var postUserPasswordCalledWith: Pair<String, String>? = null
+        private set
+
+    var deleteUserAccountResult: Result<Unit> = Result.Success(Unit)
+    var deleteUserAccountCalled = false
+        private set
+
+    var fetchUserInfoResult: Result<Unit> = Result.Success(Unit)
+    var fetchUserInfoCalled = false
+        private set
+
+    var performLogoutResult: Result<Unit> = Result.Success(Unit)
+    var performLogoutCalled = false
+        private set
+
     // endregion
 
     override suspend fun findIdByEmail(email: String): Result<Unit> {
@@ -39,27 +75,54 @@ class FakeUserRepository : UserRepository {
 
     override suspend fun postSignIn(id: String, password: String): Result<Unit> = TODO()
     override suspend fun postSignUp(id: String, password: String, email: String): Result<Unit> = TODO()
-    override suspend fun fetchUserInfo(): Result<Unit> = TODO()
+    override suspend fun fetchUserInfo(): Result<Unit> {
+        fetchUserInfoCalled = true
+        return fetchUserInfoResult
+    }
     override suspend fun patchUserInfo(nickname: String): Result<Unit> = TODO()
-    override suspend fun deleteUserAccount(): Result<Unit> = TODO()
-    override suspend fun putUserPassword(oldPassword: String, newPassword: String): Result<Unit> = TODO()
-    override suspend fun postUserPassword(id: String, password: String): Result<Unit> = TODO()
+    override suspend fun deleteUserAccount(): Result<Unit> {
+        deleteUserAccountCalled = true
+        return deleteUserAccountResult
+    }
+    override suspend fun putUserPassword(oldPassword: String, newPassword: String): Result<Unit> {
+        putUserPasswordCalledWith = oldPassword to newPassword
+        return putUserPasswordResult
+    }
+    override suspend fun postUserPassword(id: String, password: String): Result<Unit> {
+        postUserPasswordCalledWith = id to password
+        return postUserPasswordResult
+    }
     override suspend fun postFeedback(email: String, detail: String): Result<Unit> {
         postFeedbackCalledWith = email to detail
         return postFeedbackResult
     }
     override suspend fun postForceLogout(): Result<Unit> = TODO()
     override suspend fun getAccessToken(): Result<String> = TODO()
-    override suspend fun performLogout(): Result<Unit> = TODO()
+    override suspend fun performLogout(): Result<Unit> {
+        performLogoutCalled = true
+        return performLogoutResult
+    }
     override suspend fun registerToken(): Result<Unit> = TODO()
     override suspend fun setThemeMode(mode: ThemeMode): Result<Unit> {
         setThemeModeCalledWith = mode
         return setThemeModeResult
     }
-    override suspend fun checkEmailById(id: String): Result<String> = TODO()
-    override suspend fun sendPwResetCodeToEmail(email: String): Result<Unit> = TODO()
-    override suspend fun verifyPwResetCode(id: String, code: String): Result<Unit> = TODO()
-    override suspend fun resetPassword(id: String, password: String, code: String): Result<Unit> = TODO()
+    override suspend fun checkEmailById(id: String): Result<String> {
+        checkEmailByIdCalledWith = id
+        return checkEmailByIdResult
+    }
+    override suspend fun sendPwResetCodeToEmail(email: String): Result<Unit> {
+        sendPwResetCodeToEmailCalledWith = email
+        return sendPwResetCodeToEmailResult
+    }
+    override suspend fun verifyPwResetCode(id: String, code: String): Result<Unit> {
+        verifyPwResetCodeCalledWith = id to code
+        return verifyPwResetCodeResult
+    }
+    override suspend fun resetPassword(id: String, password: String, code: String): Result<Unit> {
+        resetPasswordCalledWith = Triple(id, password, code)
+        return resetPasswordResult
+    }
     override suspend fun sendCodeToEmail(email: String): Result<Unit> = TODO()
     override suspend fun verifyEmailCode(code: String): Result<Unit> = TODO()
     override suspend fun getAccessTokenByAuthCode(authCode: String, clientId: String, clientSecret: String): Result<String> = TODO()
