14a1eef7d9e264d6fad1a05dce8e57cbbc93fdce
양주현 <didwngus01@naver.com>
2026-04-05 16:12:23 +0900

    test: Fake 구현체 구현

---
+++ app/src/test/java/com/wafflestudio/snutt2/fake/FakeVacancyRepository.kt
@@ -0,0 +1,47 @@
+package com.wafflestudio.snutt2.fake
+
+import com.wafflestudio.snutt2.data.vacancy_noti.VacancyRepository
+import com.wafflestudio.snutt2.domainmodel.Lecture
+import com.wafflestudio.snutt2.domainmodel.SearchedLecture
+import com.wafflestudio.snutt2.lib.network.Result
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class FakeVacancyRepository : VacancyRepository {
+
+    // --- StateFlow ---
+    override val firstVacancyVisit = MutableStateFlow(false)
+    override val firstVacancyAdd = MutableStateFlow(false)
+    override val vacancyLectures = MutableStateFlow<List<SearchedLecture>>(emptyList())
+
+    // --- 테스트 제어용 필드 ---
+    var fetchVacancyLecturesResult: Result<Unit> = Result.Success(Unit)
+
+    var removeVacancyLectureResult: Result<Unit> = Result.Success(Unit)
+    var removeVacancyLectureCalledWith: MutableList<Lecture> = mutableListOf()
+        private set
+
+    var setVacancyVisitedResult: Result<Unit> = Result.Success(Unit)
+    var setVacancyVisitedCalled = false
+        private set
+
+    // --- 인터페이스 구현 ---
+    override suspend fun fetchVacancyLectures(): Result<Unit> {
+        return fetchVacancyLecturesResult
+    }
+
+    override suspend fun removeVacancyLecture(lecture: Lecture): Result<Unit> {
+        removeVacancyLectureCalledWith.add(lecture)
+        return removeVacancyLectureResult
+    }
+
+    override suspend fun setVacancyVisited(): Result<Unit> {
+        setVacancyVisitedCalled = true
+        return setVacancyVisitedResult
+    }
+
+    // --- 미사용 메서드 ---
+    override suspend fun addVacancyLecture(lecture: Lecture): Result<Unit> = TODO("Not used in this test")
+    override suspend fun isVacancyRegistered(lecture: Lecture): Result<Boolean> = TODO("Not used in this test")
+    override suspend fun isLectureVacancyRegistered(lecture: Lecture): Result<Boolean> = TODO("Not used in this test")
+    override suspend fun setVacancyAdded() = TODO("Not used in this test")
+}
