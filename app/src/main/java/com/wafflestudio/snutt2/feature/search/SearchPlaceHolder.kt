bba07f61f60b96a0a313b4ee2ae9437a9edd19bb
qdrptd <99246971+qdrptd@users.noreply.github.com>
2026-06-27 10:09:00 +0900

    검색 꿀팁 영어 문구 변경 (#627)

--- app/src/main/java/com/wafflestudio/snutt2/feature/search/SearchPlaceHolder.kt
+++ app/src/main/java/com/wafflestudio/snutt2/feature/search/SearchPlaceHolder.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.wafflestudio.snutt2.R
@@ -45,6 +46,7 @@ fun SearchPlaceHolder(
         Text(
             text = stringResource(R.string.search_result_placeholder_1),
             style = SNUTTTypography.h1.copy(fontSize = 25.sp, color = SNUTTColors.White700),
+            textAlign = TextAlign.Center,
         )
         Spacer(modifier = Modifier.height(35.dp))
         Text(
